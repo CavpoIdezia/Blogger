7ce59864a7008121bd69f652680d66357caf9d7f
iddeltanet <redjack.squad@gmail.com>
2016-08-28 21:24:33 +0700

    2017

---
+++ MotoGP/2017/MotoGP.2017.Rider.js
@@ -0,0 +1,205 @@
+/* Rider MotoGP 2017 */
+var nam1EsA		= "Aleix Espargaro";
+var nam1Low		= "Sam Lowes";
+var nam1Dov		= "Andrea Dovizioso";
+var nam1Lor		= "Jorge Lorenzo";
+var nam1OP1		= "TBA";
+var nam1OP2		= "TBA";
+var nam1AR1		= "TBA";
+var nam1AR2		= "TBA";
+var nam1EG1		= "TBA";
+var nam1EG2		= "TBA";
+var nam1Ped		= "Dani Pedrosa";
+var nam1Maq		= "Marc Marquez";
+var nam1Cru		= "Cal Crutchlow";
+var nam1Smi		= "Bradley Smith";
+var nam1EsP		= "Pol Espargaro";
+var nam1Ian		= "Andrea Iannone";
+var nam1Rin		= "Alex Rins";
+var nam1AT1		= "TBA";
+var nam1AT2		= "TBA";
+var nam1MY1		= "TBA";
+var nam1Fol		= "Jonas Folger";
+var nam1Vin		= "Maverick Vinales";
+var nam1Ros		= "Valentino Rossi";
+
+/* Nom Rider MotoGP 2017 */
+var nom1EsA		= "41";
+var nom1Low		= "22";
+var nom1Dov		= "04";
+var nom1Lor		= "99";
+var nom1OP1		= "TBA";
+var nom1OP2		= "TBA";
+var nom1AR1		= "TBA";
+var nom1AR2		= "TBA";
+var nom1EG1		= "TBA";
+var nom1EG2		= "TBA";
+var nom1Ped		= "26";
+var nom1Maq		= "93";
+var nom1Cru		= "35";
+var nom1Smi		= "38";
+var nom1EsP		= "44";
+var nom1Ian		= "29";
+var nom1Rin		= "TBA";
+var nom1AT1		= "TBA";
+var nom1AT2		= "TBA";
+var nom1MY1		= "TBA";
+var nom1Fol		= "94";
+var nom1Vin		= "25";
+var nom1Ros		= "46";
+
+/* Rider MotoGP 2017 */
+var rid1EsA		= ""+flg1esp+" "+nam1EsA+"";
+var rid1Low		= ""+flg1gbr+" "+nam1Low+"";
+var rid1Dov		= ""+flg1ita+" "+nam1Dov+"";
+var rid1Lor		= ""+flg1esp+" "+nam1Lor+"";
+var rid1OP1		= ""+flg1no1+" "+nam1OP1+"";
+var rid1OP2		= ""+flg1no1+" "+nam1OP2+"";
+var rid1AR1		= ""+flg1no1+" "+nam1AR1+"";
+var rid1AR2		= ""+flg1no1+" "+nam1AR2+"";
+var rid1EG1		= ""+flg1no1+" "+nam1EG1+"";
+var rid1EG2		= ""+flg1no1+" "+nam1EG2+"";
+var rid1Ped		= ""+flg1esp+" "+nam1Ped+"";
+var rid1Maq		= ""+flg1esp+" "+nam1Maq+"";
+var rid1Cru		= ""+flg1gbr+" "+nam1Cru+"";
+var rid1Smi		= ""+flg1gbr+" "+nam1Smi+"";
+var rid1EsP		= ""+flg1esp+" "+nam1EsP+"";
+var rid1Ian		= ""+flg1ita+" "+nam1Ian+"";
+var rid1Rin		= ""+flg1esp+" "+nam1Rin+"";
+var rid1AT1		= ""+flg1no1+" "+nam1AT1+"";
+var rid1AT2		= ""+flg1no1+" "+nam1AT2+"";
+var rid1MY1		= ""+flg1no1+" "+nam1MY1+"";
+var rid1Fol		= ""+flg1ger+" "+nam1Fol+"";
+var rid1Vin		= ""+flg1esp+" "+nam1Vin+"";
+var rid1Ros		= ""+flg1ita+" "+nam1Ros+"";
+
+/* Tim MotoGP 2017 */
+var nomApr1		= ""+nom1EsA+"";
+var nomApr2		= ""+nom1Low+"";
+var nomDuc1		= ""+nom1Dov+"";
+var nomDuc2		= ""+nom1Lor+"";
+var nomOct1		= ""+nom1OP1+"";
+var nomOct2		= ""+nom1OP2+"";
+var nomAvi1		= ""+nom1AR1+"";
+var nomAvi2		= ""+nom1AR2+"";
+var nomEst1		= ""+nom1EG1+"";
+var nomEst2		= ""+nom1EG2+"";
+var nomHon1		= ""+nom1Ped+"";
+var nomHon2		= ""+nom1Maq+"";
+var nomLCR1		= ""+nom1Cru+"";
+var nomKTM1		= ""+nom1Smi+"";
+var nomKTM2		= ""+nom1EsP+"";
+var nomSuz1		= ""+nom1Ian+"";
+var nomSuz2		= ""+nom1Rin+"";
+var nomAsp1		= ""+nom1AT1+"";
+var nomAsp2		= ""+nom1AT2+"";
+var nomMon1		= ""+nom1MY1+"";
+var nomMon2		= ""+nom1Fol+"";
+var nomYam1		= ""+nom1Vin+"";
+var nomYam2		= ""+nom1Ros+"";
+
+/* Tim MotoGP 2017 */
+var ridApr1		= ""+rid1EsA+"";
+var ridApr2		= ""+rid1Low+"";
+var ridDuc1		= ""+rid1Dov+"";
+var ridDuc2		= ""+rid1Lor+"";
+var ridOct1		= ""+rid1OP1+"";
+var ridOct2		= ""+rid1OP2+"";
+var ridAvi1		= ""+rid1AR1+"";
+var ridAvi2		= ""+rid1AR2+"";
+var ridEst1		= ""+rid1EG1+"";
+var ridEst2		= ""+rid1EG1+"";
+var ridHon1		= ""+rid1Ped+"";
+var ridHon2		= ""+rid1Maq+"";
+var ridLCR1		= ""+rid1Cru+"";
+var ridKTM1		= ""+rid1Smi+"";
+var ridKTM2		= ""+rid1EsP+"";
+var ridSuz1		= ""+rid1Ian+"";
+var ridSuz2		= ""+rid1Rin+"";
+var ridAsp1		= ""+rid1AT1+"";
+var ridAsp2		= ""+rid1AT2+"";
+var ridMon1		= ""+rid1MY1+"";
+var ridMon2		= ""+rid1Fol+"";
+var ridYam1		= ""+rid1Vin+"";
+var ridYam2		= ""+rid1Ros+"";
+
+/* Tim MotoGP 2017 */
+var timlApr		= "Aprilia Racing Team Gresini";
+var timlDuc		= "Ducati Team";
+var timlOct		= "Octo Pramac Yakhnich";
+var timlAvi		= "Avintia Racing";
+var timlEst		= "Estrella Galicia 0,0 Marc VDS";
+var timlHon		= "Repsol Honda Team";
+var timlLCR		= "Team LCR";
+var timlKTM		= "Red Bull KTM Motorsport";
+var timlSuz		= "Team Suzuki Ecstar";
+var timlAsp		= "Aspar Team MotoGP";
+var timlMon		= "Monster Yamaha Tech 3";
+var timlYam		= "Movistar Yamaha MotoGP";
+
+/* con MotoGP 2017 */
+var conlApr		= "Aprilia";
+var conlDuc		= "Ducati";
+var conlOct		= "Ducati";
+var conlAvi		= "Ducati";
+var conlEst		= "Honda";
+var conlHon		= "Honda";
+var conlLCR		= "Honda";
+var conlKTM		= "KTM";
+var conlSuz		= "Suzuki";
+var conlAsp		= "TBA";
+var conlMon		= "Yamaha";
+var conlYam		= "Yamaha";
+
+/* con MotoGP 2017 */
+var flglApr		= ""+flg1ita+"";
+var flglDuc		= ""+flg1ita+"";
+var flglOct		= ""+flg1ita+"";
+var flglAvi		= ""+flg1esp+"";
+var flglEst		= ""+flg1bel+"";
+var flglHon		= ""+flg1jpn+"";
+var flglLCR		= ""+flg1pol+"";
+var flglKTM		= ""+flg1aut+"";
+var flglSuz		= ""+flg1jpn+"";
+var flglAsp		= ""+flg1esp+"";
+var flglMon		= ""+flg1fra+"";
+var flglYam		= ""+flg1jpn+"";
+
+/* mot MotoGP 2017 */
+var motlApr		= "Aprilia RS-GP";
+var motlDuc		= "Ducati Desmosedici GP17";
+var motlOct		= "Ducati Desmosedici GP16";
+var motlAvi		= "Ducati Desmosedici GP15";
+var motlEst		= "Honda RC213V";
+var motlHon		= "Honda RC213V";
+var motlLCR		= "Honda RC213V";
+var motlKTM		= "KTM RC16";
+var motlSuz		= "Suzuki GSX-RR";
+var motlAsp		= "TBA";
+var motlMon		= "Yamaha YZR-M1";
+var motlYam		= "Yamaha YZR-M1";
+
+/* Rider Tim MotoGP 2017 */
+var tim1EsA		= ""+timlApr+"";
+var tim1Low		= ""+timlApr+"";
+var tim1Dov		= ""+timlDuc+"";
+var tim1Lor		= ""+timlDuc+"";
+var tim1OP1		= ""+timlOct+"";
+var tim1OP2		= ""+timlOct+"";
+var tim1AR1		= ""+timlAvi+"";
+var tim1AR2		= ""+timlAvi+"";
+var tim1EG1		= ""+timlEst+"";
+var tim1EG2		= ""+timlEst+"";
+var tim1Ped		= ""+timlHon+"";
+var tim1Maq		= ""+timlHon+"";
+var tim1Cru		= ""+timlLCR+"";
+var tim1Smi		= ""+timlKTM+"";
+var tim1EsP		= ""+timlKTM+"";
+var tim1Ian		= ""+timlSuz+"";
+var tim1Rin		= ""+timlSuz+"";
+var tim1AT1		= ""+timlAsp+"";
+var tim1AT2		= ""+timlAsp+"";
+var tim1MY1		= ""+timlMon+"";
+var tim1Fol		= ""+timlMon+"";
+var tim1Vin		= ""+timlYam+"";
+var tim1Ros		= ""+timlYam+"";
