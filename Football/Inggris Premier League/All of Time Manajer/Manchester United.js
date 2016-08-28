38d2c98871c32f6d1768e43a62c33835123942fb
iddeltanet <redjack.squad@gmail.com>
2016-08-28 21:00:40 +0700

    y

---
+++ Football/Inggris Premier League/All of Time Manajer/Manchester United.js
@@ -0,0 +1,225 @@
+sbg1 = "<tr><td>";
+sbg2 = "<\/td><td>";
+sbg3 = "<\/td><\/tr>";
+flg1 = "";
+man  = "";
+
+namcl = "Manchester United F.C.";
+
+nam00 = "Unknown";
+nat00 = ""+flg1no1+"";
+fro00 = "1878";
+unt00 = "1892";
+hon00 = "";
+man00 = ""+sbg1+""+nat00+" "+nam00+""+sbg2+""+fro00+""+sbg2+""+unt00+""+sbg2+""+hon00+""+sbg3+"";
+
+
+nam01 = "<b>A. H. Albut<\/b>";
+nat01 = ""+flg1eng+"";
+fro01 = "1892";
+unt01 = "1900";
+hon01 = "";
+man01 = ""+sbg1+""+nat01+" "+nam01+""+sbg2+""+fro01+""+sbg2+""+unt01+""+sbg2+""+hon01+""+sbg3+"";
+
+
+nam02 = "<b>James West<\/b>";
+nat02 = ""+flg1eng+"";
+fro02 = "1900";
+unt02 = "September 1903";
+hon02 = "";
+man02 = ""+sbg1+""+nat02+" "+nam02+""+sbg2+""+fro02+""+sbg2+""+unt02+""+sbg2+""+hon02+""+sbg3+"";
+
+
+nam03 = "<b>Ernest Mangnall<\/b>";
+nat03 = ""+flg1eng+"";
+fro03 = "10 October 1903";
+unt03 = "9 September 1912";
+hon03 = "2 <b>First Division<\/b> titles<br \/>1 <b>FA Cup<\/b><br \/>2 <b>Charity Shields<\/b>";
+man03 = ""+sbg1+""+nat03+" "+nam03+""+sbg2+""+fro03+""+sbg2+""+unt03+""+sbg2+""+hon03+""+sbg3+"";
+
+
+nam04 = "<b>T. J. Wallworth<\/b>";
+nat04 = ""+flg1no1+"";
+fro04 = "9 September 1912";
+unt04 = "20 October 1912";
+hon04 = "";
+man04 = ""+sbg1+""+nat04+" "+nam04+""+sbg2+""+fro04+""+sbg2+""+unt04+""+sbg2+""+hon04+""+sbg3+"";
+
+
+nam05 = "<b>John Bentley<\/b>";
+nat05 = ""+flg1eng+"";
+fro05 = "28 October 1912";
+unt05 = "28 December 1914";
+hon05 = "";
+man05 = ""+sbg1+""+nat05+" "+nam05+""+sbg2+""+fro05+""+sbg2+""+unt05+""+sbg2+""+hon05+""+sbg3+"";
+
+
+nam06 = "<b>Jack Robson<\/b>";
+nat06 = ""+flg1eng+"";
+fro06 = "28 December 1914";
+unt06 = "31 October 1921";
+hon06 = "";
+man06 = ""+sbg1+""+nat06+" "+nam06+""+sbg2+""+fro06+""+sbg2+""+unt06+""+sbg2+""+hon06+""+sbg3+"";
+
+
+nam07 = "<b>John Chapman<\/b>";
+nat07 = ""+flg1sco+"";
+fro07 = "31 October 1921";
+unt07 = "8 October 1926";
+hon07 = "";
+man07 = ""+sbg1+""+nat07+" "+nam07+""+sbg2+""+fro07+""+sbg2+""+unt07+""+sbg2+""+hon07+""+sbg3+"";
+
+
+nam08 = "<b>Lal Hilditch<\/b>";
+nat08 = ""+flg1eng+"";
+fro08 = "8 October 1926";
+unt08 = "13 April 1927";
+hon08 = "";
+man08 = ""+sbg1+""+nat08+" "+nam08+""+sbg2+""+fro08+""+sbg2+""+unt08+""+sbg2+""+hon08+""+sbg3+"";
+
+
+nam09 = "<b>Herbert Bamlett";
+nat09 = ""+flg1eng+"";
+fro09 = "13 April 1927";
+unt09 = "9 November 1931";
+hon09 = "";
+man09 = ""+sbg1+""+nat09+" "+nam09+""+sbg2+""+fro09+""+sbg2+""+unt09+""+sbg2+""+hon09+""+sbg3+"";
+
+
+nam10 = "<b>Walter Crickmer<\/b>";
+nat10 = ""+flg1eng+"";
+fro10 = "9 November 1931";
+unt10 = "13 July 1932";
+hon10 = "";
+man10 = ""+sbg1+""+nat10+" "+nam10+""+sbg2+""+fro10+""+sbg2+""+unt10+""+sbg2+""+hon10+""+sbg3+"";
+
+
+nam11 = "<b>Scott Duncan<\/b>";
+nat11 = ""+flg1sco+"";
+fro11 = "13 July 1932";
+unt11 = "7 November 1937";
+hon11 = "1 <b>Second Division<\/b> title";
+man11 = ""+sbg1+""+nat11+" "+nam11+""+sbg2+""+fro11+""+sbg2+""+unt11+""+sbg2+""+hon11+""+sbg3+"";
+
+
+nam12 = "<b>Walter Crickmer<\/b>";
+nat12 = ""+flg1eng+"";
+fro12 = "9 November 1937";
+unt12 = "15 February 1945";
+hon12 = "";
+man12 = ""+sbg1+""+nat12+" "+nam12+""+sbg2+""+fro12+""+sbg2+""+unt12+""+sbg2+""+hon12+""+sbg3+"";
+
+
+nam13 = "<b>Matt Busby<\/b>";
+nat13 = ""+flg1sco+"";
+fro13 = "1 October 1945";
+unt13 = "4 June 1969";
+hon13 = "5 <b>First Division<\/b> titles<br \/>2 <b>FA Cups<\/b><br \/>5 <b>Charity Shields<\/b><br \/>1 <b>European Cup<\/b>";
+man13 = ""+sbg1+""+nat13+" "+nam13+""+sbg2+""+fro13+""+sbg2+""+unt13+""+sbg2+""+hon13+""+sbg3+"";
+
+
+nam14 = "<b>Jimmy Murphy<\/b>";
+nat14 = ""+flg1wal+"";
+fro14 = "February 1958";
+unt14 = "June 1958";
+hon14 = "";
+man14 = ""+sbg1+""+nat14+" "+nam14+""+sbg2+""+fro14+""+sbg2+""+unt14+""+sbg2+""+hon14+""+sbg3+"";
+
+
+nam15 = "<b>Wilf McGuinness<\/b>";
+nat15 = ""+flg1eng+"";
+fro15 = "4 June 1969";
+unt15 = "29 December 1970";
+hon15 = "";
+man15 = ""+sbg1+""+nat15+" "+nam15+""+sbg2+""+fro15+""+sbg2+""+unt15+""+sbg2+""+hon15+""+sbg3+"";
+
+
+nam16 = "<b>Matt Busby<\/b>";
+nat16 = ""+flg1sco+"";
+fro16 = "29 December 1970";
+unt16 = "8 June 1971";
+hon16 = "";
+man16 = ""+sbg1+""+nat16+" "+nam16+""+sbg2+""+fro16+""+sbg2+""+unt16+""+sbg2+""+hon16+""+sbg3+"";
+
+
+nam17 = "<b>Frank O'Farrell<\/b>";
+nat17 = ""+flg1irl+"";
+fro17 = "8 June 1971";
+unt17 = "19 December 1972";
+hon17 = "";
+man17 = ""+sbg1+""+nat17+" "+nam17+""+sbg2+""+fro17+""+sbg2+""+unt17+""+sbg2+""+hon17+""+sbg3+"";
+
+
+nam18 = "<b>Tommy Docherty<\/b>";
+nat18 = ""+flg1sco+"";
+fro18 = "22 December 1972";
+unt18 = "4 July 1977";
+hon18 = "1 <b>FA Cup<\/b><br \/>1 <b>Second Division<\/b> title";
+man18 = ""+sbg1+""+nat18+" "+nam18+""+sbg2+""+fro18+""+sbg2+""+unt18+""+sbg2+""+hon18+""+sbg3+"";
+
+
+nam19 = "<b>Dave Sexton<\/b>";
+nat19 = ""+flg1eng+"";
+fro19 = "14 July 1977";
+unt19 = "30 April 1981";
+hon19 = "1 <b>Charity Shield<\/b> (shared)";
+man19 = ""+sbg1+""+nat19+" "+nam19+""+sbg2+""+fro19+""+sbg2+""+unt19+""+sbg2+""+hon19+""+sbg3+"";
+
+
+nam20 = "<b>Ron Atkinson<\/b>";
+nat20 = ""+flg1eng+"";
+fro20 = "9 June 1981";
+unt20 = "6 November 1986";
+hon20 = "2 <b>FA Cups<\/b><br \/>1 <b>Charity Shield<\/b>";
+man20 = ""+sbg1+""+nat20+" "+nam20+""+sbg2+""+fro20+""+sbg2+""+unt20+""+sbg2+""+hon20+""+sbg3+"";
+
+
+nam21 = "<b>Alex Ferguson<\/b>";
+nat21 = ""+flg1sco+"";
+fro21 = "6 November 1986";
+unt21 = "19 May 2013";
+hon21 = "13 <b>Premier League<\/b> titles<br \/>5 <b>FA Cups<\/b><br \/>4 <b>League Cups<\/b><br \/>10 <b>Community Shields<\/b><br \/>2 <b>UEFA Champions Leagues<\/b><br \/>1 <b>UEFA Cup Winners' Cup<\/b><br \/>1 <b>UEFA Super Cup<\/b><br \/>1 <b>Intercontinental Cup<\/b><br \/>1 <b>FIFA Club World Cup<\/b>";
+man21 = ""+sbg1+""+nat21+" "+nam21+""+sbg2+""+fro21+""+sbg2+""+unt21+""+sbg2+""+hon21+""+sbg3+"";
+
+
+nam22 = "<b>David Moyes<\/b>";
+nat22 = ""+flg1sco+"";
+fro22 = "1 July 2013";
+unt22 = "22 April 2014";
+hon22 = "1 <b>Community Shield<\/b>";
+man22 = ""+sbg1+""+nat22+" "+nam22+""+sbg2+""+fro22+""+sbg2+""+unt22+""+sbg2+""+hon22+""+sbg3+"";
+
+
+nam23 = "<b>Ryan Giggs<\/b> (interim)";
+nat23 = ""+flg1wal+"";
+fro23 = "22 April 2014";
+unt23 = "11 May 2014";
+hon23 = "";
+man23 = ""+sbg1+""+nat23+" "+nam23+""+sbg2+""+fro23+""+sbg2+""+unt23+""+sbg2+""+hon23+""+sbg3+"";
+
+
+nam24 = "<b>Louis van Gaal<\/b>";
+nat24 = ""+flg1ned+"";
+fro24 = "16 July 2014";
+unt24 = "23 May 2016";
+hon24 = "1 <b>FA Cup<\/b>";
+man24 = ""+sbg1+""+nat24+" "+nam24+""+sbg2+""+fro24+""+sbg2+""+unt24+""+sbg2+""+hon24+""+sbg3+"";
+
+
+nam25 = "<b>Jose Mourinho<\/b>";
+nat25 = ""+flg1por+"";
+fro25 = "27 May 2016";
+unt25 = "Present";
+hon25 = "";
+man25 = ""+sbg1+""+nat25+" "+nam25+""+sbg2+""+fro25+""+sbg2+""+unt25+""+sbg2+""+hon25+""+sbg3+"";
+
+
+nam26 = "";
+nat26 = ""+flg1+"";
+fro26 = "";
+unt26 = "";
+hon26 = "";
+man26 = ""+sbg1+""+nat26+" "+nam26+""+sbg2+""+fro26+""+sbg2+""+unt26+""+sbg2+""+hon26+""+sbg3+"";
+
+
+manager = ""+man00+""+man01+""+man02+""+man03+""+man04+""+man05+""+man06+""+man07+""+man08+""+man09+""+man10+""+man11+""+man12+""+man13+""+man14+""+man15+""+man16+""+man17+""+man18+""+man19+""+man20+""+man21+""+man22+""+man23+""+man24+""+man25+"";
