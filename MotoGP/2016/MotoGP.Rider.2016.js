3cf5bd4fba4dd688db800387c6eb41ae3330a832
iddeltanet <redjack.squad@gmail.com>
2016-08-28 19:37:41 +0700

    yes

---
+++ MotoGP/2016/MotoGP.Rider.2016.js
@@ -0,0 +1,182 @@
+var sbg1	= "<img alt=\"";
+var sbg2	= "\" title=\"";
+var sbg3	= "\" src=\"";
+var sbg4	= "\" width=\"170\" \/>";
+
+var flg1afg	= ""+sbg1+""+neg1afg+""+sbg2+""+neg1afg+""+sbg3+"http://2.bp.blogspot.com/-5G4DJh78TeY/VjdTOR1XvAI/AAAAAAAAA8g/RHshImsNROg/s1600/afg.png"+sbg4+"";	
+
+/* Rider MotoGP 2016 */
+var nam1Dov		= "Andrea Dovizioso";
+var nam1Ian		= "Andrea Iannone";
+var nam1Pir		= "Michele Pirro";
+var nam1Pet		= "Danilo Petrucci";
+var nam1Red		= "Scott Redding";
+var nam1Brd		= "Stefan Bradl";
+var nam1Bau		= "Alvaro Bautista";
+var nam1Bar		= "Hector Barbera";
+var nam1Baz		= "Loris Baz";
+var nam1Vin		= "Maverick Vinales";
+var nam1EsA		= "Aleix Espargaro";
+var nam1Ped		= "Dani Pedrosa";
+var nam1Maq		= "Marc Marquez";
+var nam1Cru		= "Cal Crutchlow";
+var nam1Smi		= "Bradley Smith";
+var nam1EsP		= "Pol Espargaro";
+var nam1Mil		= "Jack Miller";
+var nam1Rab		= "Esteve Rabat";
+var nam1Ros		= "Valentino Rossi";
+var nam1Lor		= "Jorge Lorenzo";
+var nam1Lav		= "Eugene Laverty";
+var nam1Her		= "Yonny Hernandez";	
+
+/* Rider MotoGP 2016 */
+var tim1Dov		= "Ducati Team";
+var tim1Ian		= "Ducati Team";
+var tim1Pir		= "Ducati";
+var tim1Pet		= "Octo Pramac Yakhnich";
+var tim1Red		= "Octo Pramac Yakhnich";
+var tim1Brd		= "Aprilia Racing Team Gresini";
+var tim1Bau		= "Aprilia Racing Team Gresini";
+var tim1Bar		= "Avintia Racing";
+var tim1Baz		= "Avintia Racing";
+var tim1Vin		= "Team Suzuki Ecstar";
+var tim1EsA		= "Team Suzuki Ecstar";
+var tim1Ped		= "Repsol Honda Team";
+var tim1Maq		= "Repsol Honda Team";
+var tim1Cru		= "LCR Honda";
+var tim1Smi		= "Monster Yamaha Tech 3";
+var tim1EsP		= "Monster Yamaha Tech 3";
+var tim1Mil		= "Estrella Galicia 0,0 Marc VDS";
+var tim1Rab		= "Estrella Galicia 0,0 Marc VDS";
+var tim1Ros		= "Movistar Yamaha MotoGP";
+var tim1Lor		= "Movistar Yamaha MotoGP";
+var tim1Lav		= "Aspar Team MotoGP";
+var tim1Her		= "Aspar Team MotoGP";
+
+/* Foto Rider MotoGP 2016 */
+var fot1Dov		= "https://4.bp.blogspot.com/-S0jgUJAmUyQ/V1_4Ew-Xa4I/AAAAAAAAB8o/tS9tRjJ_IFoHHRL8Ip4qcio-1AIW2zOEgCLcB/s1600/Dov.jpg";
+var fot1Ian		= "https://4.bp.blogspot.com/-Ibyz5_QCJBc/V1_4GN6OkyI/AAAAAAAAB88/kInsxNkRix88IAdE1APyfq8h54U5c7Z1QCLcB/s1600/Ian.jpg";
+var fot1Pir		= "https://3.bp.blogspot.com/-rhRoDip_t8I/V1_4HePI0pI/AAAAAAAAB9c/3VYRiDR-a3Q1L9v0J7V-mr5rIygJ5XtPgCLcB/s1600/Pir.jpg";
+var fot1Pet		= "https://1.bp.blogspot.com/-JSpHTnCrfaE/V1_4Hf3t3sI/AAAAAAAAB9U/eq3XaQObKHQEeW6OxfiC2PtD3vea_dYgQCLcB/s1600/Pet.jpg";
+var fot1Red		= "https://2.bp.blogspot.com/-uiW6xUuN2fU/V1_4IA5Z2wI/AAAAAAAAB9o/WeIlXTiBouw_54YXktm4xbZWf9j5RwMqQCLcB/s1600/Red.jpg";
+var fot1Brd		= "https://4.bp.blogspot.com/-sZ2oLz5a5Kw/V1_4E9MyaiI/AAAAAAAAB8s/Bg9kJ3n6DOcrWXqiF4Gqu6ck7_K8VmRIQCLcB/s1600/Brd.jpg";
+var fot1Bau		= "https://1.bp.blogspot.com/-UrJOncKJ-qc/V1_4EXsBIQI/AAAAAAAAB8g/TDkdXc4swgILPjORSQTrO71p3drFbOf6ACLcB/s1600/Bau.jpg";
+var fot1Bar		= "https://1.bp.blogspot.com/-iKMoKXnKxMY/V1_4EPzO_TI/AAAAAAAAB8c/hdUPac_Q0WoxiB6a3o6JAtJzL8BoaaL6wCLcB/s1600/Bar.jpg";
+var fot1Baz		= "https://1.bp.blogspot.com/-ib-uybPKuYc/V1_4ELjOL_I/AAAAAAAAB8Y/HKeuUk84MgYY3KGRt4MX1qheWrlbTcWxACLcB/s1600/Baz.jpg";
+var fot1Vin		= "https://1.bp.blogspot.com/-R3KWeN8etso/V1_4JJkfGZI/AAAAAAAAB9s/c72j-xEJOpkBWwAUCc_PrdCczyrvmIH0wCLcB/s1600/Vin.jpg";
+var fot1EsA		= "https://3.bp.blogspot.com/-EB4bUqL9Tww/V1_4FeijCbI/AAAAAAAAB8w/-j256s-mwxEYMp00zhqKQDhhxfmCueWLACLcB/s1600/EsA.jpg";
+var fot1Ped		= "https://1.bp.blogspot.com/-amdbqvjb8kg/V1_4G4tmDtI/AAAAAAAAB9M/VzspyEHcKxUF7V-w8XMmsoNjJxgBI8YbwCLcB/s1600/Ped.jpg";
+var fot1Maq		= "https://3.bp.blogspot.com/-0CSPyIVW73U/V1_4GpjZMuI/AAAAAAAAB9Q/OX1Ny1CRvhASDRLWcsyq1JXY-V4SeEb2gCLcB/s1600/Maq.jpg";
+var fot1Cru		= "https://2.bp.blogspot.com/-DgYZbIvq3y4/V1_4E_qquwI/AAAAAAAAB8k/MIw_wcvHowQNcpx_0XQ5lIlMA2LBJtFzgCLcB/s1600/Cru.jpg";
+var fot1Smi		= "https://4.bp.blogspot.com/-LdRnnmhuy7Q/V1_4IY5aIaI/AAAAAAAAB9k/xrMVLxgjioUvMMPSKjZyn3PszCUb3eJgwCLcB/s1600/Smi.jpg";
+var fot1EsP		= "https://4.bp.blogspot.com/-h6YJjrXi71c/V1_4Ftk9QcI/AAAAAAAAB80/jfYCaRLLy0E0ahmw7KRrhzBWlD_2-hLEwCLcB/s1600/EsP.jpg";
+var fot1Mil		= "https://2.bp.blogspot.com/-IfRT-ERDxdE/V1_4G2N26mI/AAAAAAAAB9I/TeoMt3ax7Z4ScJLAVSRQkqn0fFiErrIoQCLcB/s1600/Mil.jpg";
+var fot1Rab		= "https://2.bp.blogspot.com/-DmmBhv68PFw/V1_4HZ5N0YI/AAAAAAAAB9Y/amDLrQ_ZGEMgzLJ-OUYsJivuh9FGbe7rACLcB/s1600/Rab.jpg";
+var fot1Ros		= "https://1.bp.blogspot.com/-KlPV592jJe4/V1_4IYOJ5CI/AAAAAAAAB9g/k7xIPgciaw4kF7Cx6TvgLS_yOb-00MUHgCLcB/s1600/Ros.jpg";
+var fot1Lor		= "https://3.bp.blogspot.com/-UcQRi2JZFJg/V1_4GK9unfI/AAAAAAAAB9A/jBppwV8VGbAiVNgEVjBDizXk_KUCU7RFQCLcB/s1600/Lor.jpg";
+var fot1Lav		= "https://1.bp.blogspot.com/-Hp9Hsgw5_MA/V1_4GJak0uI/AAAAAAAAB9E/kP9qSPAWIQ8HNr9zYGpcgz0Y48VC4gSvQCLcB/s1600/Lav.jpg";
+var fot1Her		= "https://4.bp.blogspot.com/-pj4dwzQkmCA/V1_4FqDYPsI/AAAAAAAAB84/xxS4GJz8AlUN3ZnpOCWgKzeObVWCQqstgCLcB/s1600/Her.jpg";	
+
+/* Foto Podium MotoGP 2016 */
+var pod1Dov		= ""+sbg1+""+nam1Dov+""+sbg2+""+nam1Dov+""+sbg3+""+fot1Dov+""+sbg4+"";
+var pod1Ian		= ""+sbg1+""+nam1Ian+""+sbg2+""+nam1Ian+""+sbg3+""+fot1Ian+""+sbg4+"";
+var pod1Pir		= ""+sbg1+""+nam1Pir+""+sbg2+""+nam1Pir+""+sbg3+""+fot1Pir+""+sbg4+"";
+var pod1Pet		= ""+sbg1+""+nam1Pet+""+sbg2+""+nam1Pet+""+sbg3+""+fot1Pet+""+sbg4+"";
+var pod1Red		= ""+sbg1+""+nam1Red+""+sbg2+""+nam1Red+""+sbg3+""+fot1Red+""+sbg4+"";
+var pod1Brd		= ""+sbg1+""+nam1Brd+""+sbg2+""+nam1Brd+""+sbg3+""+fot1Brd+""+sbg4+"";
+var pod1Bau		= ""+sbg1+""+nam1Bau+""+sbg2+""+nam1Bau+""+sbg3+""+fot1Bau+""+sbg4+"";
+var pod1Bar		= ""+sbg1+""+nam1Bar+""+sbg2+""+nam1Bar+""+sbg3+""+fot1Bar+""+sbg4+"";
+var pod1Baz		= ""+sbg1+""+nam1Baz+""+sbg2+""+nam1Baz+""+sbg3+""+fot1Baz+""+sbg4+"";
+var pod1Vin		= ""+sbg1+""+nam1Vin+""+sbg2+""+nam1Vin+""+sbg3+""+fot1Vin+""+sbg4+"";
+var pod1EsA		= ""+sbg1+""+nam1EsA+""+sbg2+""+nam1EsA+""+sbg3+""+fot1EsA+""+sbg4+"";
+var pod1Ped		= ""+sbg1+""+nam1Ped+""+sbg2+""+nam1Ped+""+sbg3+""+fot1Ped+""+sbg4+"";
+var pod1Maq		= ""+sbg1+""+nam1Maq+""+sbg2+""+nam1Maq+""+sbg3+""+fot1Maq+""+sbg4+"";
+var pod1Cru		= ""+sbg1+""+nam1Cru+""+sbg2+""+nam1Cru+""+sbg3+""+fot1Cru+""+sbg4+"";
+var pod1Smi		= ""+sbg1+""+nam1Smi+""+sbg2+""+nam1Smi+""+sbg3+""+fot1Smi+""+sbg4+"";
+var pod1EsP		= ""+sbg1+""+nam1EsP+""+sbg2+""+nam1EsP+""+sbg3+""+fot1EsP+""+sbg4+"";
+var pod1Mil		= ""+sbg1+""+nam1Mil+""+sbg2+""+nam1Mil+""+sbg3+""+fot1Mil+""+sbg4+"";
+var pod1Rab		= ""+sbg1+""+nam1Rab+""+sbg2+""+nam1Rab+""+sbg3+""+fot1Rab+""+sbg4+"";
+var pod1Ros		= ""+sbg1+""+nam1Ros+""+sbg2+""+nam1Ros+""+sbg3+""+fot1Ros+""+sbg4+"";
+var pod1Lor		= ""+sbg1+""+nam1Lor+""+sbg2+""+nam1Lor+""+sbg3+""+fot1Lor+""+sbg4+"";
+var pod1Lav		= ""+sbg1+""+nam1Lav+""+sbg2+""+nam1Lav+""+sbg3+""+fot1Lav+""+sbg4+"";
+var pod1Her		= ""+sbg1+""+nam1Her+""+sbg2+""+nam1Her+""+sbg3+""+fot1Her+""+sbg4+"";
+
+/* Rider MotoGP 2016 */
+var rid1Dov		= ""+flg1ita+" "+nam1Dov+"";
+var rid1Ian		= ""+flg1ita+" "+nam1Ian+"";
+var rid1Pir		= ""+flg1ita+" "+nam1Pir+"";
+var rid1Pet		= ""+flg1ita+" "+nam1Pet+"";
+var rid1Red		= ""+flg1gbr+" "+nam1Red+"";
+var rid1Brd		= ""+flg1ger+" "+nam1Brd+"";
+var rid1Bau		= ""+flg1esp+" "+nam1Bau+"";
+var rid1Bar		= ""+flg1esp+" "+nam1Bar+"";
+var rid1Baz		= ""+flg1fra+" "+nam1Baz+"";
+var rid1Vin		= ""+flg1esp+" "+nam1Vin+"";
+var rid1EsA		= ""+flg1esp+" "+nam1EsA+"";
+var rid1Ped		= ""+flg1esp+" "+nam1Ped+"";
+var rid1Maq		= ""+flg1esp+" "+nam1Maq+"";
+var rid1Cru		= ""+flg1gbr+" "+nam1Cru+"";
+var rid1Smi		= ""+flg1gbr+" "+nam1Smi+"";
+var rid1EsP		= ""+flg1esp+" "+nam1EsP+"";
+var rid1Mil		= ""+flg1aus+" "+nam1Mil+"";
+var rid1Rab		= ""+flg1esp+" "+nam1Rab+"";
+var rid1Ros		= ""+flg1ita+" "+nam1Ros+"";
+var rid1Lor		= ""+flg1esp+" "+nam1Lor+"";
+var rid1Lav		= ""+flg1irl+" "+nam1Lav+"";
+var rid1Her		= ""+flg1col+" "+nam1Her+"";
+
+/* Nomor Rider MotoGP 2016 */
+var nom1Dov		= "04";
+var nom1Ian		= "29";
+var nom1Pir		= "51";
+var nom1Pet		= "9";
+var nom1Red		= "45";
+var nom1Brd		= "6";
+var nom1Bau		= "19";
+var nom1Bar		= "8";
+var nom1Baz		= "76";
+var nom1Vin		= "25";
+var nom1EsA		= "41";
+var nom1Ped		= "26";
+var nom1Maq		= "93";
+var nom1Cru		= "35";
+var nom1Smi		= "38";
+var nom1EsP		= "44";
+var nom1Mil		= "43";
+var nom1Rab		= "53";
+var nom1Ros		= "46";
+var nom1Lor		= "99";
+var nom1Lav		= "50";
+var nom1Her		= "68";
+
+/* Constructor */
+var consYam		= ""+flg1jpn+" Yamaha";
+var consHon		= ""+flg1jpn+" Honda";
+var consDuc		= ""+flg1ita+" Ducati";
+var consApr		= ""+flg1ita+" Aprilia";
+var consSuz		= ""+flg1jpn+" Suzuki";
+
+
+/* Motor Rider MotoGP 2016 */
+var mot1Dov		= "Ducati";
+var mot1Ian		= "Ducati";
+var mot1Pir		= "Ducati";
+var mot1Pet		= "Ducati";
+var mot1Red		= "Ducati";
+var mot1Brd		= "Aprilia";
+var mot1Bau		= "Aprilia";
+var mot1Bar		= "Ducati";
+var mot1Baz		= "Ducati";
+var mot1Vin		= "Suzuki";
+var mot1EsA		= "Suzuki";
+var mot1Ped		= "Honda";
+var mot1Maq		= "Honda";
+var mot1Cru		= "Honda";
+var mot1Smi		= "Yamaha";
+var mot1EsP		= "Yamaha";
+var mot1Mil		= "Honda";
+var mot1Rab		= "Honda";
+var mot1Ros		= "Yamaha";
+var mot1Lor		= "Yamaha";
+var mot1Lav		= "Ducati";
+var mot1Her		= "Ducati";
