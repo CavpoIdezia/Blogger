93680711fcba457691144e6a0ed435c7ec0d28fd
iddeltanet <redjack.squad@gmail.com>
2016-08-28 20:27:46 +0700

    yes

---
+++ Formula 1/F1-2016.js
@@ -0,0 +1,116 @@
+/* granprix */
+var granprix01 = "Australian Grand Prix";
+var granprix02 = "Bahrain Grand Prix";
+var granprix03 = "Chinese Grand Prix";
+var granprix04 = "Russian Grand Prix";
+var granprix05 = "Spanish Grand Prix";
+var granprix06 = "Monaco Grand Prix";
+var granprix07 = "Canadian Grand Prix";
+var granprix08 = "European Grand Prix";
+var granprix09 = "Austrian Grand Prix";
+var granprix10 = "British Grand Prix";
+var granprix11 = "Hungarian Grand Prix";
+var granprix12 = "German Grand Prix";
+var granprix13 = "Belgian Grand Prix";
+var granprix14 = "Italian Grand Prix";
+var granprix15 = "Singapore Grand Prix";
+var granprix16 = "Malaysian Grand Prix";
+var granprix17 = "Japanese Grand Prix";
+var granprix18 = "United States Grand Prix";
+var granprix19 = "Mexican Grand Prix";
+var granprix20 = "Brazilian Grand Prix";
+var granprix21 = "Abu Dhabi Grand Prix";
+
+
+/* Sirkuit */
+var sirkuit01 = "Melbourne Grand Prix Circuit, Melbourne";
+var sirkuit02 = "Bahrain International Circuit, Sakhir";
+var sirkuit03 = "Shanghai International Circuit, Shanghai";
+var sirkuit04 = "Sochi Autodrom, Sochi";
+var sirkuit05 = "Circuit de Barcelona-Catalunya, Barcelona";
+var sirkuit06 = "Circuit de Monaco, Monte Carlo";
+var sirkuit07 = "Circuit Gilles Villeneuve, Montreal";
+var sirkuit08 = "Baku City Circuit, Baku";
+var sirkuit09 = "Red Bull Ring, Spielberg";
+var sirkuit10 = "Silverstone Circuit, Silverstone";
+var sirkuit11 = "Hungaroring, Budapest";
+var sirkuit12 = "Hockenheimring, Hockenheim";
+var sirkuit13 = "Circuit de Spa-Francorchamps, Stavelot";
+var sirkuit14 = "Autodromo Nazionale Monza, Monza";
+var sirkuit15 = "Marina Bay Street Circuit, Singapore";
+var sirkuit16 = "Sepang International Circuit, Kuala Lumpur";
+var sirkuit17 = "Suzuka International Racing Course, Suzuka";
+var sirkuit18 = "Circuit of the Americas, Austin, Texas";
+var sirkuit19 = "Autodromo Hermanos Rodriguez, Mexico City";
+var sirkuit20 = "Autodromo Jose Carlos Pace, Sao Paulo";
+var sirkuit21 = "Yas Marina Circuit, Abu Dhabi";
+
+/* tanggal */
+var tanggal01 = "20 Maret 2016";
+var tanggal02 = "3 April 2016";
+var tanggal03 = "17 April 2016";
+var tanggal04 = "1 Mey 2016";
+var tanggal05 = "15 Mey 2016";
+var tanggal06 = "29 Mey 2016";
+var tanggal07 = "12 Juni 2016";
+var tanggal08 = "19 Juni 2016";
+var tanggal09 = "3 Juli 2016";
+var tanggal10 = "10 Juli 2016";
+var tanggal11 = "24 Juli 2016";
+var tanggal12 = "31 Juli 2016";
+var tanggal13 = "28 Agustus 2016";
+var tanggal14 = "4 September 2016";
+var tanggal15 = "18 September 2016";
+var tanggal16 = "2 Oktober 2016";
+var tanggal17 = "9 Oktober 2016";
+var tanggal18 = "23 Oktober 2016";
+var tanggal19 = "30 Oktober 2016";
+var tanggal20 = "13 Nopember 2016";
+var tanggal21 = "27 Nopember 2016";
+
+
+/* gambar */
+var gambar01 = "http://1.bp.blogspot.com/-AO84SD9zDj4/VjdTR3EWZeI/AAAAAAAAA9s/vGyKZLMIxGM/s1600/aus.png";
+var gambar02 = "http://1.bp.blogspot.com/-EyOa0CKovmo/VjdTU9YANAI/AAAAAAAAA_A/q3t7-5TBj2o/s1600/bhr.png";
+var gambar03 = "http://2.bp.blogspot.com/-0Qd2tBqmLC8/VjdT6NgOzLI/AAAAAAAABA8/hzlvKAO34mc/s1600/chn.png";
+var gambar04 = "http://1.bp.blogspot.com/-1BowFQeAjfI/VjdU3iDeHjI/AAAAAAAABPg/KyIiIb1wj1g/s1600/rus.png";
+var gambar05 = "http://1.bp.blogspot.com/-Wm-fLEWfKno/VjdUB2WgbbI/AAAAAAAABD0/q9jHT4x77S8/s1600/esp.png";
+var gambar06 = "https://3.bp.blogspot.com/-ZMUUkkjAfuo/V10wQr6IMrI/AAAAAAAABzA/NqFu8hLXAnkoXEB4_jQ4deu3Zwbwh0Z7wCLcB/s1600/mon.png";
+var gambar07 = "http://4.bp.blogspot.com/-n813aj6Z6dc/VjdT4Vyag1I/AAAAAAAABAc/RX73qfbiaI8/s1600/can.png";
+var gambar08 = "http://3.bp.blogspot.com/-glk0MMan-7Y/VjdTSsGvcZI/AAAAAAAAA-A/1HCJ2E-dOFU/s1600/aze.png";
+var gambar09 = "http://2.bp.blogspot.com/-PFFNXkKoI-U/VjdTSL9aKTI/AAAAAAAAA94/BR6Q3mgmK-k/s1600/aut.png";
+var gambar10 = "http://1.bp.blogspot.com/-68UwtCBfvMs/VjdUER0x_xI/AAAAAAAABE0/EEl6eA_DCdU/s1600/gbr.png";
+var gambar11 = "http://4.bp.blogspot.com/-2V1-uB8IxwM/VjdUMCxQjTI/AAAAAAAABGo/zb9c2vbvCMQ/s1600/hun.png";
+var gambar12 = "http://2.bp.blogspot.com/--r4Kh4KqVoE/VjdUFgiFHUI/AAAAAAAABFY/b6zLWGHo8qc/s1600/ger.png";
+var gambar13 = "http://1.bp.blogspot.com/-UXO6Rao4SQk/VjdTT6X-RII/AAAAAAAAA-o/7hnelNV__9o/s1600/bel.png";
+var gambar14 = "http://1.bp.blogspot.com/-NxQ-Pdb_2J4/VjdUQIuhbGI/AAAAAAAABHo/51fJsdl7_HE/s1600/ita.png";
+var gambar15 = "http://3.bp.blogspot.com/-Opx-NqtaGrg/VjdU5RluHnI/AAAAAAAABQU/OvWdCWL-soc/s1600/sin.png";
+var gambar16 = "http://2.bp.blogspot.com/-oIZUaoJXy0c/VjdUlcBzvzI/AAAAAAAABKg/07aOMEG7e3c/s1600/mas.png";
+var gambar17 = "http://2.bp.blogspot.com/-zbnuHcQlJSU/VjdUR-rN6tI/AAAAAAAABIE/ink6sFJNVCM/s1600/jpn.png";
+var gambar18 = "http://2.bp.blogspot.com/-yJQDHHjJp1M/VjdVEbKMFDI/AAAAAAAABUc/OdvwUh-SdzU/s1600/usa.png";
+var gambar19 = "http://3.bp.blogspot.com/-Z5gMACFwmUk/VjdUmct41nI/AAAAAAAABK4/EQIBoYjz_u4/s1600/mex.png";
+var gambar20 = "http://3.bp.blogspot.com/-0Lsy4ApZ92o/VjdTW6WQugI/AAAAAAAAA_0/YyUETqF24s0/s1600/bra.png";
+var gambar21 = "http://1.bp.blogspot.com/-zCr8k2b6KMk/VjdVDDFOq3I/AAAAAAAABT8/kMuPebotwz0/s1600/uae.png";
+
+/* image */
+var image01 = "<img alt=\""+granprix01+"\" src=\""+gambar01+"\" style=\"border-style: none; padding: 0px 0px 0px 0px;\" width=\"17\">";
+var image02 = "<img alt=\""+granprix02+"\" src=\""+gambar02+"\" style=\"border-style: none; padding: 0px 0px 0px 0px;\" width=\"17\">";
+var image03 = "<img alt=\""+granprix03+"\" src=\""+gambar03+"\" style=\"border-style: none; padding: 0px 0px 0px 0px;\" width=\"17\">";
+var image04 = "<img alt=\""+granprix04+"\" src=\""+gambar04+"\" style=\"border-style: none; padding: 0px 0px 0px 0px;\" width=\"17\">";
+var image05 = "<img alt=\""+granprix05+"\" src=\""+gambar05+"\" style=\"border-style: none; padding: 0px 0px 0px 0px;\" width=\"17\">";
+var image06 = "<img alt=\""+granprix06+"\" src=\""+gambar06+"\" style=\"border-style: none; padding: 0px 0px 0px 0px;\" width=\"17\">";
+var image07 = "<img alt=\""+granprix07+"\" src=\""+gambar07+"\" style=\"border-style: none; padding: 0px 0px 0px 0px;\" width=\"17\">";
+var image08 = "<img alt=\""+granprix08+"\" src=\""+gambar08+"\" style=\"border-style: none; padding: 0px 0px 0px 0px;\" width=\"17\">";
+var image09 = "<img alt=\""+granprix09+"\" src=\""+gambar09+"\" style=\"border-style: none; padding: 0px 0px 0px 0px;\" width=\"17\">";
+var image10 = "<img alt=\""+granprix10+"\" src=\""+gambar10+"\" style=\"border-style: none; padding: 0px 0px 0px 0px;\" width=\"17\">";
+var image11 = "<img alt=\""+granprix11+"\" src=\""+gambar11+"\" style=\"border-style: none; padding: 0px 0px 0px 0px;\" width=\"17\">";
+var image12 = "<img alt=\""+granprix12+"\" src=\""+gambar12+"\" style=\"border-style: none; padding: 0px 0px 0px 0px;\" width=\"17\">";
+var image13 = "<img alt=\""+granprix13+"\" src=\""+gambar13+"\" style=\"border-style: none; padding: 0px 0px 0px 0px;\" width=\"17\">";
+var image14 = "<img alt=\""+granprix14+"\" src=\""+gambar14+"\" style=\"border-style: none; padding: 0px 0px 0px 0px;\" width=\"17\">";
+var image15 = "<img alt=\""+granprix15+"\" src=\""+gambar15+"\" style=\"border-style: none; padding: 0px 0px 0px 0px;\" width=\"17\">";
+var image16 = "<img alt=\""+granprix16+"\" src=\""+gambar16+"\" style=\"border-style: none; padding: 0px 0px 0px 0px;\" width=\"17\">";
+var image17 = "<img alt=\""+granprix17+"\" src=\""+gambar17+"\" style=\"border-style: none; padding: 0px 0px 0px 0px;\" width=\"17\">";
+var image18 = "<img alt=\""+granprix18+"\" src=\""+gambar18+"\" style=\"border-style: none; padding: 0px 0px 0px 0px;\" width=\"17\">";
+var image19 = "<img alt=\""+granprix19+"\" src=\""+gambar19+"\" style=\"border-style: none; padding: 0px 0px 0px 0px;\" width=\"17\">";
+var image20 = "<img alt=\""+granprix20+"\" src=\""+gambar20+"\" style=\"border-style: none; padding: 0px 0px 0px 0px;\" width=\"17\">";
+var image21 = "<img alt=\""+granprix21+"\" src=\""+gambar21+"\" style=\"border-style: none; padding: 0px 0px 0px 0px;\" width=\"17\">";
