8fc34bdea547ef32e08e1ee13b8b870165f203f6
iddeltanet <redjack.squad@gmail.com>
2016-08-28 20:39:56 +0700

    yes

---
+++ Tangga Lagu/Indonesia/Lagu Lebaran/Lagu.Lebaran.Database.js
@@ -0,0 +1,95 @@
+var tdoc				= "<td align=\"center\">";
+var tdcol				= "<\/td><td align=\"left\"><font size=\"+1\"><b><u>";
+var tdcoc				= "<\/td><td align=\"center\">";
+var tdc					= "<\/td>";
+var ttplg				= "<\/u><\/font><br\/><font color=\"#31A4B9\">";
+var ttpart				= "<\/font><\/b>";
+var grk1				= "<img src=\"";
+var grk2				= "\" width=\"60\" height=\"60\" \/>";
+var ytb1				= "<a rel=\"nofollow\" target=\"_blank\" href=\"https://www.youtube.com/watch?v=";
+var ytb2				= "\"><img width=\"80\" height=\"60\" src=\"http://img.youtube.com/vi/";
+var ytb3				= "/default.jpg\" \/><\/a>";
+
+
+
+/* ############ HatiYangFitriGigi ############ */
+var jHatiYangFitriGigi 			= "Hati Yang Fitri";
+var aHatiYangFitriGigi 			= "Gigi";
+var gHatiYangFitriGigi			= "https://1.bp.blogspot.com/-GhZCwRzlQbs/V11SXhVryuI/AAAAAAAABzc/6iTXAysNpx4vi7vE47FaSBkf8Hpr5xneACLcB/s1600/Gigi%2B-%2BHari%2BYang%2BFitri.jpg";
+var yHatiYangFitriGigi			= "lf2bmfXqr3I";
+var HatiYangFitriGigi			= ""+tdoc+""+grk1+""+gHatiYangFitriGigi+""+grk2+""+tdcol+""+jHatiYangFitriGigi+""+ttplg+""+aHatiYangFitriGigi+""+ttpart+""+tdcoc+""+ytb1+""+yHatiYangFitriGigi+""+ytb2+""+yHatiYangFitriGigi+""+ytb3+""+tdc+"";"";
+
+/* ############ SelamatLebaranUngu ############ */
+var jSelamatLebaranUngu 		= "Selamat Lebaran";
+var aSelamatLebaranUngu 		= "Ungu";
+var gSelamatLebaranUngu			= "https://1.bp.blogspot.com/-jo2PtHrpy1U/V11UfyQ9s7I/AAAAAAAABzo/HEgcA-tXaRs5X-78Pv76MpiwPcjAWdKMgCLcB/s1600/Ungu_Surgamu.jpg";
+var ySelamatLebaranUngu			= "1npdjb3YgP0";
+var SelamatLebaranUngu			= ""+tdoc+""+grk1+""+gSelamatLebaranUngu+""+grk2+""+tdcol+""+jSelamatLebaranUngu+""+ttplg+""+aSelamatLebaranUngu+""+ttpart+""+tdcoc+""+ytb1+""+ySelamatLebaranUngu+""+ytb2+""+ySelamatLebaranUngu+""+ytb3+""+tdc+"";
+
+/* ############ UjeLebaran ############ */
+var jUjeLebaran 		= "Selamat Hari Lebaran";
+var aUjeLebaran 		= "Jefri Al Buchori";
+var gUjeLebaran			= "https://1.bp.blogspot.com/-wkKOmSOd76w/V11hSdRfi9I/AAAAAAAAB0A/xOv_PABEDFUh922cdmpeB1HAPzFapqvQQCLcB/s1600/Jefri-Al-B.jpg";
+var yUjeLebaran			= "vXEWnpLsU3s";
+var UjeLebaran			= ""+tdoc+""+grk1+""+gUjeLebaran+""+grk2+""+tdcol+""+jUjeLebaran+""+ttplg+""+aUjeLebaran+""+ttpart+""+tdcoc+""+ytb1+""+yUjeLebaran+""+ytb2+""+yUjeLebaran+""+ytb3+""+tdc+"";
+
+/* ############ LebaranSebentarLagiAfgan ############ */
+var jLebaranSebentarLagiAfgan 		= "Lebaran Sebentar Lagi";
+var aLebaranSebentarLagiAfgan 		= "Afgan Andien Giring";
+var gLebaranSebentarLagiAfgan		= "https://3.bp.blogspot.com/-ocwVZC3UQCs/V11hS34aVEI/AAAAAAAAB0I/07-NNC82vSUHa6KwtG5IGocZyxAODoktACLcB/s1600/aga.jpg";
+var yLebaranSebentarLagiAfgan		= "ohzn87VVh00";
+var LebaranSebentarLagiAfgan		= ""+tdoc+""+grk1+""+gLebaranSebentarLagiAfgan+""+grk2+""+tdcol+""+jLebaranSebentarLagiAfgan+""+ttplg+""+aLebaranSebentarLagiAfgan+""+ttpart+""+tdcoc+""+ytb1+""+yLebaranSebentarLagiAfgan+""+ytb2+""+yLebaranSebentarLagiAfgan+""+ytb3+""+tdc+"";
+
+/* ############ SelamatHariLebaranGigi ############ */
+var jSelamatHariLebaranGigi 		= "Selamat Hari Lebaran";
+var aSelamatHariLebaranGigi 		= "Gigi";
+var gSelamatHariLebaranGigi		= "https://1.bp.blogspot.com/-GhZCwRzlQbs/V11SXhVryuI/AAAAAAAABzc/6iTXAysNpx4vi7vE47FaSBkf8Hpr5xneACLcB/s1600/Gigi%2B-%2BHari%2BYang%2BFitri.jpg";
+var ySelamatHariLebaranGigi		= "8fgoP4M-UJM";
+var SelamatHariLebaranGigi		= ""+tdoc+""+grk1+""+gSelamatHariLebaranGigi+""+grk2+""+tdcol+""+jSelamatHariLebaranGigi+""+ttplg+""+aSelamatHariLebaranGigi+""+ttpart+""+tdcoc+""+ytb1+""+ySelamatHariLebaranGigi+""+ytb2+""+ySelamatHariLebaranGigi+""+ytb3+""+tdc+"";
+
+/* ############ IdulFitriGitaGutawa ############ */
+var jIdulFitriGitaGutawa 		= "Idul Fitri";
+var aIdulFitriGitaGutawa 		= "Gita Gutawa";
+var gIdulFitriGitaGutawa		= "https://3.bp.blogspot.com/-YmlzdxqbFjM/V11hSW5_5fI/AAAAAAAABz8/yAKceCN0D30A2fAgDa7YJh-WJmBgV1W5wCLcB/s1600/Gita-Gutawa.JPG";
+var yIdulFitriGitaGutawa		= "OtaJ1N_DvYs";
+var IdulFitriGitaGutawa			= ""+tdoc+""+grk1+""+gIdulFitriGitaGutawa+""+grk2+""+tdcol+""+jIdulFitriGitaGutawa+""+ttplg+""+aIdulFitriGitaGutawa+""+ttpart+""+tdcoc+""+ytb1+""+yIdulFitriGitaGutawa+""+ytb2+""+yIdulFitriGitaGutawa+""+ytb3+""+tdc+"";
+
+/* ############ LebaranYukEbith ############ */
+var jLebaranYukEbith 			= "Lebaran Yuk";
+var aLebaranYukEbith 			= "Ebith Beat A";
+var gLebaranYukEbith			= "https://3.bp.blogspot.com/-BO_v9XMMMmA/V11hSBScjVI/AAAAAAAABz4/FxQSFwi7vjcJRJZZ0OiRiSOJOMWWILHkACLcB/s1600/Ebith%2BBeat%2BA%2B-%2BAlhamdulillah.jpg";
+var yLebaranYukEbith			= "wWcmhq5wvFc";
+var LebaranYukEbith			= ""+tdoc+""+grk1+""+gLebaranYukEbith+""+grk2+""+tdcol+""+jLebaranYukEbith+""+ttplg+""+aLebaranYukEbith+""+ttpart+""+tdcoc+""+ytb1+""+yLebaranYukEbith+""+ytb2+""+yLebaranYukEbith+""+ytb3+""+tdc+"";
+
+/* ############ SelamatHariLebaranIvanG ############ */
+var jSelamatHariLebaranIvanG 		= "Selamat Hari Lebaran";
+var aSelamatHariLebaranIvanG 		= "Ivan Gunawan & Friends";
+var gSelamatHariLebaranIvanG		= "https://4.bp.blogspot.com/-EEBAV0fnnfM/V11hTs2tMYI/AAAAAAAAB0Q/imjpGhW6SYU0Hvkaot9x2zOi4yPxmJ4NACLcB/s1600/ig.jpg";
+var ySelamatHariLebaranIvanG		= "oV0beEe44gI";
+var SelamatHariLebaranIvanG		= ""+tdoc+""+grk1+""+gSelamatHariLebaranIvanG+""+grk2+""+tdcol+""+jSelamatHariLebaranIvanG+""+ttplg+""+aSelamatHariLebaranIvanG+""+ttpart+""+tdcoc+""+ytb1+""+ySelamatHariLebaranIvanG+""+ytb2+""+ySelamatHariLebaranIvanG+""+ytb3+""+tdc+"";
+
+/* ############ SelamatHariLebaranDenny ############ */
+var jSelamatHariLebaranDenny 		= "Selamat Hari Lebaran";
+var aSelamatHariLebaranDenny 		= "Denny Malik, Puput Novel, Yani Libel's, Betharia S";
+var gSelamatHariLebaranDenny		= "https://1.bp.blogspot.com/-f3QCvPB5aw4/V11hTPO5O3I/AAAAAAAAB0M/bJgT3QMt9xAdv3NpchNG1MEaiHQqeNDTwCLcB/s1600/denny.jpg";
+var ySelamatHariLebaranDenny		= "nLUJrWjaeMg";
+var SelamatHariLebaranDenny		= ""+tdoc+""+grk1+""+gSelamatHariLebaranDenny+""+grk2+""+tdcol+""+jSelamatHariLebaranDenny+""+ttplg+""+aSelamatHariLebaranDenny+""+ttpart+""+tdcoc+""+ytb1+""+ySelamatHariLebaranDenny+""+ytb2+""+ySelamatHariLebaranDenny+""+ytb3+""+tdc+"";
+
+/* ############ SalamLebaranRadja ############ */
+var jSalamLebaranRadja 			= "Salam Lebaran";
+var aSalamLebaranRadja 			= "Radja ";
+var gSalamLebaranRadja			= "https://1.bp.blogspot.com/-ggbz2-2GMfQ/V11hSw2AUEI/AAAAAAAAB0E/1CsVyrx4ZD8j-IItzMqDL3Es5kQy7Hg2wCLcB/s1600/Radja.jpg";
+var ySalamLebaranRadja			= "EG6eUAgJDdw";
+var SalamLebaranRadja			= ""+tdoc+""+grk1+""+gSalamLebaranRadja+""+grk2+""+tdcol+""+jSalamLebaranRadja+""+ttplg+""+aSalamLebaranRadja+""+ttpart+""+tdcoc+""+ytb1+""+ySalamLebaranRadja+""+ytb2+""+ySalamLebaranRadja+""+ytb3+""+tdc+"";
+
+
+var position01	= ""+HatiYangFitriGigi+"";
+var position02	= ""+SelamatLebaranUngu+"";
+var position03	= ""+UjeLebaran+"";
+var position04	= ""+LebaranSebentarLagiAfgan+"";
+var position05	= ""+SelamatHariLebaranGigi+"";
+var position06	= ""+IdulFitriGitaGutawa+"";
+var position07	= ""+LebaranYukEbith+"";
+var position08	= ""+SelamatHariLebaranIvanG+"";
+var position09	= ""+SelamatHariLebaranDenny+"";
+var position10	= ""+SalamLebaranRadja+"";
