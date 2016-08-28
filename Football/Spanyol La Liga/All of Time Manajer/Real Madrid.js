3e6f85a8e4c29a0813ed874c5cd88d3e8ab938e5
iddeltanet <redjack.squad@gmail.com>
2016-08-28 21:37:07 +0700

    y

---
+++ Football/Spanyol La Liga/All of Time Manajer/Real Madrid.js
@@ -0,0 +1,514 @@
+sbg1 = "<tr><td>";
+sbg2 = "<\/td><td>";
+sbg3 = "<\/td><\/tr>";
+flg1 = "";
+man  = "";
+
+namcl = "Real Madrid C.F.";
+
+
+nam01 = "Arthur Johnson";
+nat01 = ""+flg1eng+"";
+fro01 = "1910";
+unt01 = "1920";
+hon01 = "1 <b>Copa del Rey<\/>";
+man01 = ""+sbg1+""+nat01+" "+nam01+""+sbg2+""+fro01+""+sbg2+""+unt01+""+sbg2+""+hon01+""+sbg3+"";
+
+
+nam02 = "Juan de Carcer";
+nat02 = ""+flg1esp+"";
+fro02 = "1920";
+unt02 = "1926";
+hon02 = "";
+man02 = ""+sbg1+""+nat02+" "+nam02+""+sbg2+""+fro02+""+sbg2+""+unt02+""+sbg2+""+hon02+""+sbg3+"";
+
+
+nam03 = "Pedro Llorente";
+nat03 = ""+flg1esp+"";
+fro03 = "1926";
+unt03 = "1926";
+hon03 = "";
+man03 = ""+sbg1+""+nat03+" "+nam03+""+sbg2+""+fro03+""+sbg2+""+unt03+""+sbg2+""+hon03+""+sbg3+"";
+
+
+nam04 = "Santiago Bernabeu Yeste";
+nat04 = ""+flg1esp+"";
+fro04 = "1926";
+unt04 = "1927";
+hon04 = "";
+man04 = ""+sbg1+""+nat04+" "+nam04+""+sbg2+""+fro04+""+sbg2+""+unt04+""+sbg2+""+hon04+""+sbg3+"";
+
+
+nam05 = "Jose Berraondo";
+nat05 = ""+flg1esp+"";
+fro05 = "1927";
+unt05 = "1929";
+hon05 = "";
+man05 = ""+sbg1+""+nat05+" "+nam05+""+sbg2+""+fro05+""+sbg2+""+unt05+""+sbg2+""+hon05+""+sbg3+"";
+
+
+nam06 = "Jose Quirante";
+nat06 = ""+flg1esp+"";
+fro06 = "1929";
+unt06 = "1930";
+hon06 = "";
+man06 = ""+sbg1+""+nat06+" "+nam06+""+sbg2+""+fro06+""+sbg2+""+unt06+""+sbg2+""+hon06+""+sbg3+"";
+
+
+nam07 = "Lippo Hertzka";
+nat07 = ""+flg1hun+"";
+fro07 = "1930";
+unt07 = "1932";
+hon07 = "1 <b>La Liga<\/b>";
+man07 = ""+sbg1+""+nat07+" "+nam07+""+sbg2+""+fro07+""+sbg2+""+unt07+""+sbg2+""+hon07+""+sbg3+"";
+
+
+nam08 = "Robert Firth";
+nat08 = ""+flg1eng+"";
+fro08 = "1932";
+unt08 = "1934";
+hon08 = "1 <b>La Liga<\/b>";
+man08 = ""+sbg1+""+nat08+" "+nam08+""+sbg2+""+fro08+""+sbg2+""+unt08+""+sbg2+""+hon08+""+sbg3+"";
+
+
+nam09 = "Francisco Bru";
+nat09 = ""+flg1esp+"";
+fro09 = "1934";
+unt09 = "1941";
+hon09 = "2 <b>Copa del Rey<\/b>";
+man09 = ""+sbg1+""+nat09+" "+nam09+""+sbg2+""+fro09+""+sbg2+""+unt09+""+sbg2+""+hon09+""+sbg3+"";
+
+
+nam10 = "Juan Armet";
+nat10 = ""+flg1esp+"";
+fro10 = "1941";
+unt10 = "September 1943";
+hon10 = "";
+man10 = ""+sbg1+""+nat10+" "+nam10+""+sbg2+""+fro10+""+sbg2+""+unt10+""+sbg2+""+hon10+""+sbg3+"";
+
+
+nam11 = "Ramon Encinas";
+nat11 = ""+flg1esp+"";
+fro11 = "September 1943";
+unt11 = "May 1945";
+hon11 = "";
+man11 = ""+sbg1+""+nat11+" "+nam11+""+sbg2+""+fro11+""+sbg2+""+unt11+""+sbg2+""+hon11+""+sbg3+"";
+
+
+nam12 = "Jacinto Quincoces";
+nat12 = ""+flg1esp+"";
+fro12 = "May 1945";
+unt12 = "March 1946";
+hon12 = "1 <b>Copa del Rey<\/b>";
+man12 = ""+sbg1+""+nat12+" "+nam12+""+sbg2+""+fro12+""+sbg2+""+unt12+""+sbg2+""+hon12+""+sbg3+"";
+
+
+nam13 = "Baltasar Albeniz";
+nat13 = ""+flg1esp+"";
+fro13 = "March 1946";
+unt13 = "April 1947";
+hon13 = "1 <b>Copa del Rey<\/b>";
+man13 = ""+sbg1+""+nat13+" "+nam13+""+sbg2+""+fro13+""+sbg2+""+unt13+""+sbg2+""+hon13+""+sbg3+"";
+
+
+nam14 = "Jacinto Quincoces";
+nat14 = ""+flg1esp+"";
+fro14 = "April 1947";
+unt14 = "January 1948";
+hon14 = "";
+man14 = ""+sbg1+""+nat14+" "+nam14+""+sbg2+""+fro14+""+sbg2+""+unt14+""+sbg2+""+hon14+""+sbg3+"";
+
+
+nam15 = "Michael Keeping";
+nat15 = ""+flg1eng+"";
+fro15 = "January 1948";
+unt15 = "October 1950";
+hon15 = "";
+man15 = ""+sbg1+""+nat15+" "+nam15+""+sbg2+""+fro15+""+sbg2+""+unt15+""+sbg2+""+hon15+""+sbg3+"";
+
+
+nam16 = "Baltasar Albeniz";
+nat16 = ""+flg1esp+"";
+fro16 = "October 1950";
+unt16 = "March 1951";
+hon16 = "";
+man16 = ""+sbg1+""+nat16+" "+nam16+""+sbg2+""+fro16+""+sbg2+""+unt16+""+sbg2+""+hon16+""+sbg3+"";
+
+
+nam17 = "Hector Scarone";
+nat17 = ""+flg1uru+"";
+fro17 = "March 1951";
+unt17 = "April 1952";
+hon17 = "";
+man17 = ""+sbg1+""+nat17+" "+nam17+""+sbg2+""+fro17+""+sbg2+""+unt17+""+sbg2+""+hon17+""+sbg3+"";
+
+
+nam18 = "Juan Antonio Ipina";
+nat18 = ""+flg1esp+"";
+fro18 = "April 1952";
+unt18 = "May 1953";
+hon18 = "";
+man18 = ""+sbg1+""+nat18+" "+nam18+""+sbg2+""+fro18+""+sbg2+""+unt18+""+sbg2+""+hon18+""+sbg3+"";
+
+
+nam19 = "Enrique Fernandez";
+nat19 = ""+flg1uru+"";
+fro19 = "May 1953";
+unt19 = "10 December 1954";
+hon19 = "1 <b>La Liga<\/b>";
+man19 = ""+sbg1+""+nat19+" "+nam19+""+sbg2+""+fro19+""+sbg2+""+unt19+""+sbg2+""+hon19+""+sbg3+"";
+
+
+nam20 = "Jose Villalonga";
+nat20 = ""+flg1esp+"";
+fro20 = "10 December 1954";
+unt20 = "June 1957";
+hon20 = "2 <b>La Liga<\/b><br \/>2 <b>European Cup<\/b>";
+man20 = ""+sbg1+""+nat20+" "+nam20+""+sbg2+""+fro20+""+sbg2+""+unt20+""+sbg2+""+hon20+""+sbg3+"";
+
+
+nam21 = "Luis Carniglia";
+nat21 = ""+flg1arg+"";
+fro21 = "June 1957";
+unt21 = "19 February 1959";
+hon21 = "1 <b>La Liga<\/b><br \/>1 <b>European Cup<\/b>";
+man21 = ""+sbg1+""+nat21+" "+nam21+""+sbg2+""+fro21+""+sbg2+""+unt21+""+sbg2+""+hon21+""+sbg3+"";
+
+
+nam22 = "Miguel Munoz";
+nat22 = ""+flg1esp+"";
+fro22 = "21 February 1959";
+unt22 = "13 April 1959";
+hon22 = "";
+man22 = ""+sbg1+""+nat22+" "+nam22+""+sbg2+""+fro22+""+sbg2+""+unt22+""+sbg2+""+hon22+""+sbg3+"";
+
+
+nam23 = "Luis Carniglia";
+nat23 = ""+flg1arg+"";
+fro23 = "13 April 1959";
+unt23 = "July 1959";
+hon23 = "1 <b>European Cup<\/b>";
+man23 = ""+sbg1+""+nat23+" "+nam23+""+sbg2+""+fro23+""+sbg2+""+unt23+""+sbg2+""+hon23+""+sbg3+"";
+
+
+nam24 = "Manuel Fleitas";
+nat24 = ""+flg1par+"";
+fro24 = "July 1959";
+unt24 = "12 April 1960";
+hon24 = "";
+man24 = ""+sbg1+""+nat24+" "+nam24+""+sbg2+""+fro24+""+sbg2+""+unt24+""+sbg2+""+hon24+""+sbg3+"";
+
+
+nam25 = "Miguel Munoz";
+nat25 = ""+flg1esp+"";
+fro25 = "13 April 1960";
+unt25 = "15 January 1974";
+hon25 = "";
+man25 = ""+sbg1+""+nat25+" "+nam25+""+sbg2+""+fro25+""+sbg2+""+unt25+""+sbg2+""+hon25+""+sbg3+"";
+
+
+nam26 = "Luis Molowny";
+nat26 = ""+flg1esp+"";
+fro26 = "15 January 1974";
+unt26 = "May 1974";
+hon26 = "";
+man26 = ""+sbg1+""+nat26+" "+nam26+""+sbg2+""+fro26+""+sbg2+""+unt26+""+sbg2+""+hon26+""+sbg3+"";
+
+
+nam27 = "Miljan Miljanic";
+nat27 = ""+flg1yug+"";
+fro27 = "May 1974";
+unt27 = "7 September 1977";
+hon27 = "2 <b>La Liga<\/b><br \/>1 <b>Copa del Rey<\/b>";
+man27 = ""+sbg1+""+nat27+" "+nam27+""+sbg2+""+fro27+""+sbg2+""+unt27+""+sbg2+""+hon27+""+sbg3+"";
+
+
+nam28 = "Luis Molowny";
+nat28 = ""+flg1esp+"";
+fro28 = "7 September 1977";
+unt28 = "June 1979";
+hon28 = "2 <b>La Liga<\/b>";
+man28 = ""+sbg1+""+nat28+" "+nam28+""+sbg2+""+fro28+""+sbg2+""+unt28+""+sbg2+""+hon28+""+sbg3+"";
+
+
+nam29 = "Vujadin Boskov";
+nat29 = ""+flg1yug+"";
+fro29 = "June 1979";
+unt29 = "29 March 1982";
+hon29 = "1 <b>La Liga<\/b><br \/>1 <b>Copa del Rey<\/b>";
+man29 = ""+sbg1+""+nat29+" "+nam29+""+sbg2+""+fro29+""+sbg2+""+unt29+""+sbg2+""+hon29+""+sbg3+"";
+
+
+nam30 = "Luis Molowny";
+nat30 = ""+flg1esp+"";
+fro30 = "29 March 1982";
+unt30 = "30 June 1982";
+hon30 = "1 <b>Copa del Rey<\/b>";
+man30 = ""+sbg1+""+nat30+" "+nam30+""+sbg2+""+fro30+""+sbg2+""+unt30+""+sbg2+""+hon30+""+sbg3+"";
+
+
+nam31 = "Alfredo Di Stefano";
+nat31 = ""+flg1arg+"";
+fro31 = "1 July 1982";
+unt31 = "22 May 1984";
+hon31 = "";
+man31 = ""+sbg1+""+nat31+" "+nam31+""+sbg2+""+fro31+""+sbg2+""+unt31+""+sbg2+""+hon31+""+sbg3+"";
+
+
+nam32 = "Amancio Amaro";
+nat32 = ""+flg1esp+"";
+fro32 = "22 May 1984";
+unt32 = "16 April 1985";
+hon32 = "";
+man32 = ""+sbg1+""+nat32+" "+nam32+""+sbg2+""+fro32+""+sbg2+""+unt32+""+sbg2+""+hon32+""+sbg3+"";
+
+
+nam33 = "Luis Molowny";
+nat33 = ""+flg1esp+"";
+fro33 = "16 April 1985";
+unt33 = "30 June 1986";
+hon33 = "1 <b>La Liga<\/b><br \/>1 <b>Copa de la Liga<\/b><br \/>2 <b>UEFA Cup<\/b>";
+man33 = ""+sbg1+""+nat33+" "+nam33+""+sbg2+""+fro33+""+sbg2+""+unt33+""+sbg2+""+hon33+""+sbg3+"";
+
+
+nam34 = "Leo Beenhakker";
+nat34 = ""+flg1ned+"";
+fro34 = "1 July 1986";
+unt34 = "30 June 1989";
+hon34 = "3 <b>La Liga<\/b><br \/>1 <b>Copa del Rey<\/b><br \/>2 <b>Supercopa de Espana<\/b>";
+man34 = ""+sbg1+""+nat34+" "+nam34+""+sbg2+""+fro34+""+sbg2+""+unt34+""+sbg2+""+hon34+""+sbg3+"";
+
+
+nam35 = "John Toshack";
+nat35 = ""+flg1wal+"";
+fro35 = "1 July 1989";
+unt35 = "19 November 1990";
+hon35 = "1 <b>La Liga<\/b>";
+man35 = ""+sbg1+""+nat35+" "+nam35+""+sbg2+""+fro35+""+sbg2+""+unt35+""+sbg2+""+hon35+""+sbg3+"";
+
+
+nam36 = "Alfredo Di Stefano";
+nat36 = ""+flg1arg+"";
+fro36 = "21 November 1990";
+unt36 = "22 March 1991";
+hon36 = "1 <b>Supercopa de Espana<\/b>";
+man36 = ""+sbg1+""+nat36+" "+nam36+""+sbg2+""+fro36+""+sbg2+""+unt36+""+sbg2+""+hon36+""+sbg3+"";
+
+
+nam37 = "Radomir Antic";
+nat37 = ""+flg1yug+"";
+fro37 = "22 March 1991";
+unt37 = "27 January 1992";
+hon37 = "";
+man37 = ""+sbg1+""+nat37+" "+nam37+""+sbg2+""+fro37+""+sbg2+""+unt37+""+sbg2+""+hon37+""+sbg3+"";
+
+
+nam38 = "Leo Beenhakker";
+nat38 = ""+flg1ned+"";
+fro38 = "27 January 1992";
+unt38 = "29 June 1992";
+hon38 = "";
+man38 = ""+sbg1+""+nat38+" "+nam38+""+sbg2+""+fro38+""+sbg2+""+unt38+""+sbg2+""+hon38+""+sbg3+"";
+
+
+nam39 = "Benito Floro";
+nat39 = ""+flg1esp+"";
+fro39 = "1 July 1992";
+unt39 = "7 March 1994";
+hon39 = "1 <b>Copa del Rey<\/b><br \/>1 <b>Supercopa de Espana<\/b>";
+man39 = ""+sbg1+""+nat39+" "+nam39+""+sbg2+""+fro39+""+sbg2+""+unt39+""+sbg2+""+hon39+""+sbg3+"";
+
+
+nam40 = "Vicente del Bosque";
+nat40 = ""+flg1esp+"";
+fro40 = "7 March 1994";
+unt40 = "30 June 1994";
+hon40 = "";
+man40 = ""+sbg1+""+nat40+" "+nam40+""+sbg2+""+fro40+""+sbg2+""+unt40+""+sbg2+""+hon40+""+sbg3+"";
+
+
+nam41 = "Jorge Valdano";
+nat41 = ""+flg1arg+"";
+fro41 = "1 July 1994";
+unt41 = "21 January 1996";
+hon41 = "1 <b>La Liga<\/b>";
+man41 = ""+sbg1+""+nat41+" "+nam41+""+sbg2+""+fro41+""+sbg2+""+unt41+""+sbg2+""+hon41+""+sbg3+"";
+
+
+nam42 = "Vicente del Bosque";
+nat42 = ""+flg1esp+"";
+fro42 = "21 January 1996";
+unt42 = "24 January 1996";
+hon42 = "";
+man42 = ""+sbg1+""+nat42+" "+nam42+""+sbg2+""+fro42+""+sbg2+""+unt42+""+sbg2+""+hon42+""+sbg3+"";
+
+
+nam43 = "Arsenio Iglesias";
+nat43 = ""+flg1esp+"";
+fro43 = "24 January 1996";
+unt43 = "29 May 1996";
+hon43 = "";
+man43 = ""+sbg1+""+nat43+" "+nam43+""+sbg2+""+fro43+""+sbg2+""+unt43+""+sbg2+""+hon43+""+sbg3+"";
+
+
+nam44 = "Fabio Capello";
+nat44 = ""+flg1ita+"";
+fro44 = "1 July 1996";
+unt44 = "24 June 1997";
+hon44 = "1 <b>La Liga<\/b>";
+man44 = ""+sbg1+""+nat44+" "+nam44+""+sbg2+""+fro44+""+sbg2+""+unt44+""+sbg2+""+hon44+""+sbg3+"";
+
+
+nam45 = "Jupp Heynckes";
+nat45 = ""+flg1ger+"";
+fro45 = "25 June 1997";
+unt45 = "28 May 1998";
+hon45 = "1 <b>Supercopa de Espana<\/b><br \/>1 <b>UEFA Champions League<\/b>";
+man45 = ""+sbg1+""+nat45+" "+nam45+""+sbg2+""+fro45+""+sbg2+""+unt45+""+sbg2+""+hon45+""+sbg3+"";
+
+
+nam46 = "Jose Antonio Camacho";
+nat46 = ""+flg1esp+"";
+fro46 = "17 June 1998";
+unt46 = "9 July 1998";
+hon46 = "";
+man46 = ""+sbg1+""+nat46+" "+nam46+""+sbg2+""+fro46+""+sbg2+""+unt46+""+sbg2+""+hon46+""+sbg3+"";
+
+
+nam47 = "Guus Hiddink";
+nat47 = ""+flg1ned+"";
+fro47 = "15 July 1998";
+unt47 = "23 February 1999";
+hon47 = "1 <b>Intercontinental Cup<\/b>";
+man47 = ""+sbg1+""+nat47+" "+nam47+""+sbg2+""+fro47+""+sbg2+""+unt47+""+sbg2+""+hon47+""+sbg3+"";
+
+
+nam48 = "John Toshack";
+nat48 = ""+flg1wal+"";
+fro48 = "24 February 1999";
+unt48 = "17 November 1999";
+hon48 = "";
+man48 = ""+sbg1+""+nat48+" "+nam48+""+sbg2+""+fro48+""+sbg2+""+unt48+""+sbg2+""+hon48+""+sbg3+"";
+
+
+nam49 = "Vicente del Bosque";
+nat49 = ""+flg1esp+"";
+fro49 = "17 November 1999";
+unt49 = "23 June 2003";
+hon49 = "1 <b>La Liga<\/b><br \/>1 <b>Supercopa de Espana<\/b><br \/>2 <b>UEFA Champions League<\/b><br \/>1 <b>UEFA Super Cup<\/b><br \/>1 <b>Intercontinental Cup<\/b>";
+man49 = ""+sbg1+""+nat49+" "+nam49+""+sbg2+""+fro49+""+sbg2+""+unt49+""+sbg2+""+hon49+""+sbg3+"";
+
+
+nam50 = "Carlos Queiroz";
+nat50 = ""+flg1por+"";
+fro50 = "25 June 2003";
+unt50 = "24 May 2004";
+hon50 = "1 <b>Supercopa de Espana<\/b>";
+man50 = ""+sbg1+""+nat50+" "+nam50+""+sbg2+""+fro50+""+sbg2+""+unt50+""+sbg2+""+hon50+""+sbg3+"";
+
+
+nam51 = "Jose Antonio Camacho";
+nat51 = ""+flg1esp+"";
+fro51 = "25 May 2004";
+unt51 = "20 September 2004";
+hon51 = "";
+man51 = ""+sbg1+""+nat51+" "+nam51+""+sbg2+""+fro51+""+sbg2+""+unt51+""+sbg2+""+hon51+""+sbg3+"";
+
+
+nam52 = "Mariano Garcia Remon";
+nat52 = ""+flg1esp+"";
+fro52 = "20 September 2004";
+unt52 = "30 December 2004";
+hon52 = "";
+man52 = ""+sbg1+""+nat52+" "+nam52+""+sbg2+""+fro52+""+sbg2+""+unt52+""+sbg2+""+hon52+""+sbg3+"";
+
+
+nam53 = "Vanderlei Luxemburgo";
+nat53 = ""+flg1bra+"";
+fro53 = "30 December 2004";
+unt53 = "4 December 2005";
+hon53 = "";
+man53 = ""+sbg1+""+nat53+" "+nam53+""+sbg2+""+fro53+""+sbg2+""+unt53+""+sbg2+""+hon53+""+sbg3+"";
+
+
+nam54 = "Juan Ramon Lopez Caro";
+nat54 = ""+flg1esp+"";
+fro54 = "4 December 2005";
+unt54 = "1 June 2006";
+hon54 = "";
+man54 = ""+sbg1+""+nat54+" "+nam54+""+sbg2+""+fro54+""+sbg2+""+unt54+""+sbg2+""+hon54+""+sbg3+"";
+
+
+nam55 = "Fabio Capello";
+nat55 = ""+flg1ita+"";
+fro55 = "5 July 2006";
+unt55 = "28 June 2007";
+hon55 = "1 <b>La Liga<\/b>";
+man55 = ""+sbg1+""+nat55+" "+nam55+""+sbg2+""+fro55+""+sbg2+""+unt55+""+sbg2+""+hon55+""+sbg3+"";
+
+
+nam56 = "Bernd Schuster";
+nat56 = ""+flg1ger+"";
+fro56 = "9 July 2007";
+unt56 = "9 December 2008";
+hon56 = "1 <b>La Liga<\/b><br \/>1 <b>Supercopa de Espana<\/b>";
+man56 = ""+sbg1+""+nat56+" "+nam56+""+sbg2+""+fro56+""+sbg2+""+unt56+""+sbg2+""+hon56+""+sbg3+"";
+
+
+nam57 = "Juande Ramos";
+nat57 = ""+flg1esp+"";
+fro57 = "9 December 2008";
+unt57 = "1 June 2009";
+hon57 = "";
+man57 = ""+sbg1+""+nat57+" "+nam57+""+sbg2+""+fro57+""+sbg2+""+unt57+""+sbg2+""+hon57+""+sbg3+"";
+
+
+nam58 = "Manuel Pellegrini";
+nat58 = ""+flg1chi+"";
+fro58 = "2 June 2009";
+unt58 = "26 May 2010";
+hon58 = "";
+man58 = ""+sbg1+""+nat58+" "+nam58+""+sbg2+""+fro58+""+sbg2+""+unt58+""+sbg2+""+hon58+""+sbg3+"";
+
+
+nam59 = "Jose Mourinho";
+nat59 = ""+flg1por+"";
+fro59 = "31 May 2010";
+unt59 = "1 June 2013";
+hon59 = "1 <b>La Liga<\/b><br \/>1 <b>Copa del Rey<\/b><br \/>1 <b>Supercopa de Espana<\/b>";
+man59 = ""+sbg1+""+nat59+" "+nam59+""+sbg2+""+fro59+""+sbg2+""+unt59+""+sbg2+""+hon59+""+sbg3+"";
+
+
+nam60 = "Carlo Ancelotti";
+nat60 = ""+flg1ita+"";
+fro60 = "25 June 2013";
+unt60 = "25 May 2015";
+hon60 = "1 <b>La Liga<\/b><br \/>1 <b>UEFA Champions League<\/b><br \/>1 <b>UEFA Super Cup<\/b><br \/>1 <b>FIFA Club World Cup<\/b>";
+man60 = ""+sbg1+""+nat60+" "+nam60+""+sbg2+""+fro60+""+sbg2+""+unt60+""+sbg2+""+hon60+""+sbg3+"";
+
+
+nam61 = "Rafael Benitez";
+nat61 = ""+flg1esp+"";
+fro61 = "3 June 2015";
+unt61 = "4 January 2016";
+hon61 = "";
+man61 = ""+sbg1+""+nat61+" "+nam61+""+sbg2+""+fro61+""+sbg2+""+unt61+""+sbg2+""+hon61+""+sbg3+"";
+
+
+nam62 = "Zinedine Zidane";
+nat62 = ""+flg1fra+"";
+fro62 = "4 January 2016";
+unt62 = "Present";
+hon62 = "1 <b>UEFA Champions League<\/b>";
+man62 = ""+sbg1+""+nat62+" "+nam62+""+sbg2+""+fro62+""+sbg2+""+unt62+""+sbg2+""+hon62+""+sbg3+"";
+
+
+nam63 = "";
+nat63 = ""+flg1+"";
+fro63 = "";
+unt63 = "";
+hon63 = "";
+man63 = ""+sbg1+""+nat63+" "+nam63+""+sbg2+""+fro63+""+sbg2+""+unt63+""+sbg2+""+hon63+""+sbg3+"";
+
+
+manager = ""+man01+""+man02+""+man03+""+man04+""+man05+""+man06+""+man07+""+man08+""+man09+""+man10+""+man11+""+man12+""+man13+""+man14+""+man15+""+man16+""+man17+""+man18+""+man19+""+man20+""+man21+""+man22+""+man23+""+man24+""+man25+""+man26+""+man27+""+man28+""+man29+""+man30+""+man31+""+man32+""+man33+""+man34+""+man35+""+man36+""+man37+""+man38+""+man39+""+man40+""+man41+""+man42+""+man43+""+man44+""+man45+""+man46+""+man47+""+man48+""+man49+""+man50+""+man51+""+man52+""+man53+""+man54+""+man55+""+man56+""+man57+""+man58+""+man59+""+man60+""+man61+""+man62+""+man+""+man+"";
