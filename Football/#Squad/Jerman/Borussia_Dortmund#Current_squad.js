3462bfb229db9baec524de946e00dff5f261b23c
CavpoIdezia <smithbradley370@gmail.com>
2016-09-01 23:36:25 +0700

    gertrasep

--- Football/#Squad/Jerman/Borussia_Dortmund#Current_squad.js
+++ Football/#Squad/Jerman/Borussia_Dortmund#Current_squad.js
@@ -1,9 +1,8 @@
 ﻿sq1	= "<tr><td align=\"center\">1<\/td><td align=\"center\">GK<\/td><td>"+flg1ger+" <b>Roman Weidenfeller<\/b><\/td><\/tr>";
 sq3	= "<tr><td align=\"center\">3<\/td><td align=\"center\">DF<\/td><td>"+flg1kor+" <b>Park Joo-ho<\/b><\/td><\/tr>";
-sq4	= "<tr><td align=\"center\">4<\/td><td align=\"center\">DF<\/td><td>"+flg1srb+" <b>Neven Subotić<\/b><\/td><\/tr>";
 sq5	= "<tr><td align=\"center\">5<\/td><td align=\"center\">DF<\/td><td>"+flg1esp+" <b>Marc Bartra<\/b><\/td><\/tr>";
 sq6	= "<tr><td align=\"center\">6<\/td><td align=\"center\">MF<\/td><td>"+flg1ger+" <b>Sven Bender<\/b><\/td><\/tr>";
-sq7	= "<tr><td align=\"center\">7<\/td><td align=\"center\">FW<\/td><td>"+flg1fra+" <b>Ousmane Dembélé<\/b><\/td><\/tr>";
+sq7	= "<tr><td align=\"center\">7<\/td><td align=\"center\">MF<\/td><td>"+flg1fra+" <b>Ousmane Dembélé<\/b><\/td><\/tr>";
 sq8	= "<tr><td align=\"center\">8<\/td><td align=\"center\">MF<\/td><td>"+flg1tur+" <b>Nuri Şahin<\/b><\/td><\/tr>";
 sq9	= "<tr><td align=\"center\">9<\/td><td align=\"center\">MF<\/td><td>"+flg1tur+" <b>Emre Mor<\/b><\/td><\/tr>";
 sq10	= "<tr><td align=\"center\">10<\/td><td align=\"center\">MF<\/td><td>"+flg1ger+" <b>Mario Götze<\/b><\/td><\/tr>";
@@ -22,12 +21,13 @@ sq27	= "<tr><td align=\"center\">27<\/td><td align=\"center\">MF<\/td><td>"+flg1
 sq28	= "<tr><td align=\"center\">28<\/td><td align=\"center\">DF<\/td><td>"+flg1ger+" <b>Matthias Ginter<\/b><\/td><\/tr>";
 sq29	= "<tr><td align=\"center\">29<\/td><td align=\"center\">DF<\/td><td>"+flg1ger+" <b>Marcel Schmelzer<\/b><\/td><\/tr>";
 sq30	= "<tr><td align=\"center\">30<\/td><td align=\"center\">MF<\/td><td>"+flg1ger+" <b>Felix Passlack<\/b><\/td><\/tr>";
-sq32	= "<tr><td align=\"center\">32<\/td><td align=\"center\">MF<\/td><td>"+flg1ger+" <b>Dženis Burnić<\/b><\/td><\/tr>";
 sq33	= "<tr><td align=\"center\">33<\/td><td align=\"center\">MF<\/td><td>"+flg1ger+" <b>Julian Weigl<\/b><\/td><\/tr>";
-sq35	= "<tr><td align=\"center\">35<\/td><td align=\"center\">GK<\/td><td>"+flg1ger+" <b>Jan-Pascal Reckert<\/b><\/td><\/tr>";
 sq37	= "<tr><td align=\"center\">37<\/td><td align=\"center\">DF<\/td><td>"+flg1ger+" <b>Erik Durm<\/b><\/td><\/tr>";
 sq38	= "<tr><td align=\"center\">38<\/td><td align=\"center\">GK<\/td><td>"+flg1sui+" <b>Roman Bürki<\/b><\/td><\/tr>";
 sq39	= "<tr><td align=\"center\">39<\/td><td align=\"center\">GK<\/td><td>"+flg1ger+" <b>Hendrik Bonmann<\/b><\/td><\/tr>";
+	
+	
+	
 	
 	
 	
@@ -80,7 +80,7 @@ captain	= "<br>"+flg1ger+" <b>Marcel Schmelzer<\/b> (<i>Captain<\/i>)";
 	
 	
 	
-lastud	= "26 "+Aug+" 2016";
-table1	= ""+sq1+""+sq3+""+sq4+""+sq5+""+sq6+""+sq7+""+sq8+""+sq9+""+sq10+""+sq11+""+sq13+""+sq17+""+sq18+""+sq20+""+sq21+"";
-table2	= ""+sq22+""+sq23+""+sq24+""+sq25+""+sq26+""+sq27+""+sq28+""+sq29+""+sq30+""+sq32+""+sq33+""+sq35+""+sq37+""+sq38+""+sq39+"";
+lastud	= "(Closing Transfer, 1 "+Sep+" 2016)";
+table1	= ""+sq1+""+sq3+""+sq5+""+sq6+""+sq7+""+sq8+""+sq9+""+sq10+""+sq11+""+sq13+""+sq17+""+sq18+""+sq20+""+sq21+"";
+table2	= ""+sq22+""+sq23+""+sq24+""+sq25+""+sq26+""+sq27+""+sq28+""+sq29+""+sq30+""+sq33+""+sq37+""+sq38+""+sq39+"<tr><td colspan=\"3\" align=\"center\">-<\/td><\/tr>";
 notetim	= ""+captain+"";
