37b094bb51540ed4e0496c19ae19a1d68aae36be
CavpoIdezia <smithbradley370@gmail.com>
2016-09-02 23:10:04 +0700

    traporsep

--- Football/#Squad/Portugal/S.L._Benfica#First-team_squad.js
+++ Football/#Squad/Portugal/S.L._Benfica#First-team_squad.js
@@ -3,12 +3,11 @@ sq2	= "<tr><td align=\"center\">2<\/td><td align=\"center\">DF<\/td><td>"+flg1ar
 sq3	= "<tr><td align=\"center\">3<\/td><td align=\"center\">DF<\/td><td>"+flg1esp+" <b>Álex Grimaldo<\/b><\/td><\/tr>";
 sq4	= "<tr><td align=\"center\">4<\/td><td align=\"center\">DF<\/td><td>"+flg1bra+" <b>Luisão<\/b><\/td><\/tr>";
 sq5	= "<tr><td align=\"center\">5<\/td><td align=\"center\">MF<\/td><td>"+flg1srb+" <b>Ljubomir Fejsa<\/b><\/td><\/tr>";
-sq6	= "<tr><td align=\"center\">6<\/td><td align=\"center\">MF<\/td><td>"+flg1por+" <b>Rúben Amorim<\/b><\/td><\/tr>";
 sq7	= "<tr><td align=\"center\">7<\/td><td align=\"center\">MF<\/td><td>"+flg1gre+" <b>Andreas Samaris<\/b><\/td><\/tr>";
 sq8	= "<tr><td align=\"center\">8<\/td><td align=\"center\">MF<\/td><td>"+flg1por+" <b>André Horta<\/b><\/td><\/tr>";
 sq9	= "<tr><td align=\"center\">9<\/td><td align=\"center\">FW<\/td><td>"+flg1mex+" <b>Raúl Jiménez<\/b><\/td><\/tr>";
 sq10	= "<tr><td align=\"center\">10<\/td><td align=\"center\">FW<\/td><td>"+flg1bra+" <b>Jonas<\/b><\/td><\/tr>";
-sq11	= "<tr><td align=\"center\">11<\/td><td align=\"center\">FW<\/td><td>"+flg1gre+" <b>Konstantinos Mitroglou<\/b><\/td><\/tr>";
+sq11	= "<tr><td align=\"center\">11<\/td><td align=\"center\">FW<\/td><td>"+flg1gre+" <b>K. Mitroglou<\/b><\/td><\/tr>";
 sq12	= "<tr><td align=\"center\">12<\/td><td align=\"center\">GK<\/td><td>"+flg1bra+" <b>Júlio César<\/b><\/td><\/tr>";
 sq13	= "<tr><td align=\"center\">13<\/td><td align=\"center\">GK<\/td><td>"+flg1por+" <b>Paulo Lopes<\/b><\/td><\/tr>";
 sq14	= "<tr><td align=\"center\">14<\/td><td align=\"center\">DF<\/td><td>"+flg1swe+" <b>Victor Lindelöf<\/b><\/td><\/tr>";
@@ -20,21 +19,22 @@ sq19	= "<tr><td align=\"center\">19<\/td><td align=\"center\">DF<\/td><td>"+flg1
 sq20	= "<tr><td align=\"center\">20<\/td><td align=\"center\">FW<\/td><td>"+flg1por+" <b>Gonçalo Guedes<\/b><\/td><\/tr>";
 sq21	= "<tr><td align=\"center\">21<\/td><td align=\"center\">MF<\/td><td>"+flg1por+" <b>Pizzi<\/b><\/td><\/tr>";
 sq22	= "<tr><td align=\"center\">22<\/td><td align=\"center\">MF<\/td><td>"+flg1arg+" <b>Franco Cervi<\/b><\/td><\/tr>";
-sq23	= "<tr><td align=\"center\">23<\/td><td align=\"center\">MF<\/td><td>"+flg1arg+" <b>Oscar Benítez<\/b><\/td><\/tr>";
-sq25	= "<tr><td align=\"center\">25<\/td><td align=\"center\">FW<\/td><td>"+flg1por+" <b>Rui Fonte<\/b><\/td><\/tr>";
-sq27	= "<tr><td align=\"center\">27<\/td><td align=\"center\">DF<\/td><td>"+flg1bra+" <b>Marçal<\/b><\/td><\/tr>";
 sq28	= "<tr><td align=\"center\">28<\/td><td align=\"center\">MF<\/td><td>"+flg1col+" <b>Guillermo Celis<\/b><\/td><\/tr>";
-sq30	= "<tr><td align=\"center\">30<\/td><td align=\"center\">MF<\/td><td>"+flg1bra+" <b>Talisca<\/b><\/td><\/tr>";
 sq33	= "<tr><td align=\"center\">33<\/td><td align=\"center\">DF<\/td><td>"+flg1bra+" <b>Jardel<\/b><\/td><\/tr>";
 sq34	= "<tr><td align=\"center\">34<\/td><td align=\"center\">MF<\/td><td>"+flg1por+" <b>André Almeida<\/b><\/td><\/tr>";
 sq35	= "<tr><td align=\"center\">35<\/td><td align=\"center\">FW<\/td><td>"+flg1srb+" <b>Luka Jović<\/b><\/td><\/tr>";
 sq37	= "<tr><td align=\"center\">37<\/td><td align=\"center\">MF<\/td><td>"+flg1bra+" <b>Danilo<\/b><\/td><\/tr>";
-sq39	= "<tr><td align=\"center\">39<\/td><td align=\"center\">MF<\/td><td>"+flg1mar+" <b>Mehdi C.-González<\/b><\/td><\/tr>";
 sq50	= "<tr><td align=\"center\">50<\/td><td align=\"center\">DF<\/td><td>"+flg1por+" <b>Nélson Semedo<\/b><\/td><\/tr>";
-sqx1	= "<tr><td align=\"center\">—<\/td><td align=\"center\">DF<\/td><td>"+flg1can+" <b>Steven Vitória<\/b><\/td><\/tr>";
-sqx2	= "<tr><td align=\"center\">—<\/td><td align=\"center\">MF<\/td><td>"+flg1ned+" <b>Ola John<\/b><\/td><\/tr>";
-sqx3	= "<tr><td align=\"center\">—<\/td><td align=\"center\">MF<\/td><td>"+flg1por+" <b>Pelé<\/b><\/td><\/tr>";
-sqx4	= "<tr><td align=\"center\">—<\/td><td align=\"center\">FW<\/td><td>"+flg1por+" <b>Nélson Oliveira<\/b><\/td><\/tr>";
+sqx1	= "<tr><td align=\"center\">—<\/td><td align=\"center\">MF<\/td><td>"+flg1por+" <b>Rafa<\/b><\/td><\/tr>";
+sqx2	= "<tr><td align=\"center\">—<\/td><td align=\"center\">MF<\/td><td>"+flg1por+" <b>Rúben Amorim<\/b><\/td><\/tr>";
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
@@ -80,7 +80,7 @@ loan1	= "<br>"+flg1bra+" <b>Danilo<\/b> (<i>"+onloan+" <a href=\"http://www.idez
 	
 	
 	
-lastud	= "14 "+Aug+" 2016";
-table1	= ""+sq1+""+sq2+""+sq3+""+sq4+""+sq5+""+sq6+""+sq7+""+sq8+""+sq9+""+sq10+""+sq11+""+sq12+""+sq13+""+sq14+""+sq15+""+sq16+""+sq17+""+sq18+""+sq19+"";
-table2	= ""+sq20+""+sq21+""+sq22+""+sq23+""+sq25+""+sq27+""+sq28+""+sq30+""+sq33+""+sq34+""+sq35+""+sq37+""+sq39+""+sq50+""+sqx1+""+sqx2+""+sqx3+""+sqx4+"<tr><td colspan=\"3\" align=\"center\">-<\/td><\/tr>";
+lastud	= "(Closing Transfer, 1 "+Sep+" 2016)";
+table1	= ""+sq1+""+sq2+""+sq3+""+sq4+""+sq5+""+sq7+""+sq8+""+sq9+""+sq10+""+sq11+""+sq12+""+sq13+""+sq14+""+sq15+""+sq16+"";
+table2	= ""+sq17+""+sq18+""+sq19+""+sq20+""+sq21+""+sq22+""+sq28+""+sq33+""+sq34+""+sq35+""+sq37+""+sq50+""+sqx1+""+sqx2+"<tr><td colspan=\"3\" align=\"center\">-<\/td><\/tr>";
 notetim	= ""+captain+""+vicecap+""+thicapt+""+forcapt+""+loan1+"";
